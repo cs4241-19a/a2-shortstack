a8242e2d35c3341e6ac447cda72f8540e94fa2d0
Ken Desrosiers <kdesrosiers@wpi.edu>
2019-08-31 18:20:38 -0400

    progress

--- server.improved.js
+++ server.improved.js
@@ -1,20 +1,29 @@
 const http = require( 'http' ),
       fs   = require( 'fs' ),
+      bodyParser = require('body-parser'),
       // IMPORTANT: you must run `npm install` in the directory for this assignment
       // to install the mime library used in the following line of code
-      mime = require( 'mime' ),
       express = require('express'),
-      app = express()
-      dir  = 'public/',
-      port = 3000
+      app = express(),
+      port = process.env.PORT || 3000
 
 const appdata = [
   { 'model': 'toyota', 'year': 1999, 'mpg': 23 },
   { 'model': 'honda', 'year': 2004, 'mpg': 30 },
   { 'model': 'ford', 'year': 1987, 'mpg': 14}
 ]
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+app.use("/js", express.static(__dirname + '/public/js'));
+app.use("/css", express.static(__dirname + '/public/css'));
 app.get('/', function(req, res) {
-  res.send(appdata)
+  res.sendFile(__dirname + '/public/index.html');
+})
+app.get('/index.html', function(req, res) {
+  res.sendFile(__dirname + '/public/index.html');
+})
+app.post('/submit', function(req, res) {
+  console.log(req);
 })
 
 app.listen(port)
