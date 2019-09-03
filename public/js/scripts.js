be1129d9e49b1b1016070b48caad23ff779c6589
Ken Desrosiers <kdesrosiers@wpi.edu>
2019-09-02 22:43:26 -0400

    whole lotta stuff

--- public/js/scripts.js
+++ public/js/scripts.js
@@ -1,13 +1,100 @@
-/*function test() {
-  const input = document.querySelector( '#yourname' );
-  const json = { "yourname": input.value };
-  const body = JSON.stringify(json);
-  fetch( '/submit', {
+//to generate a new customer every so often
+function generateCustomer(){
+  var rand = random(1, 4);
+  var newEl = document.createElement('div');
+  var divString = "";
+  //var customer = document.createElement("IMG");
+  var order = document.createElement("P");
+  if (rand === 1) {
+    divString="<div style='display: inline;'><img ondrop='duringDrop(event)' ondragover='allowDragging(event)' style='height:70px; width:50px;' src='../assets/chocolate.png'/></div>";
+    newEl.innerHTML = divString;
+    order.innerHTML = "CHOCOLATE";
+  }
+  else if (rand === 2){
+    divString="<div style='display: inline;'><img ondrop='duringDrop(event)' ondragover='allowDragging(event)' style='height:70px; width:50px;' src='../assets/cookie-dough.png'/></div>";
+    newEl.innerHTML = divString;
+    order.innerHTML = "COOKIE DOUGH";
+  }
+  else if (rand === 3) {
+    divString="<div style='display: inline;'><img ondrop='duringDrop(event)' ondragover='allowDragging(event)' style='height:70px; width:50px;' src='../assets/vanilla.png'/></div>";
+    newEl.innerHTML = divString;
+    order.innerHTML = "VANILLA";
+  }
+  else if (rand === 4) {
+    divString="<div style='display: inline;'><img ondrop='duringDrop(event)' ondragover='allowDragging(event)' style='height:70px; width:50px;' src='../assets/strawberry.png'/></div>";
+    newEl.innerHTML = divString;
+    order.innerHTML = "STRAWBERRY";
+  }
+  document.querySelector("#line").appendChild(newEl.firstChild);
+  document.querySelector('#orders').appendChild(order);
+}
+
+//generate a random number
+function random(min, max){
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+
+//update my money
+function updateBank(num) {
+  const json = { amount: num},
+  body = JSON.stringify( json )
+  fetch( '/updateBank', {
     method:'POST',
+    headers: {'Content-Type': 'application/json'},
     body
   })
-  .then( function( response ) {
-    // do something with the reponse
-    console.log(response);
-  })
-}*/
+.then( promiseresponse => promiseresponse.json())
+.then(response => {
+  document.querySelector('#bills').innerHTML = response.result;
+})
+return false;
+}
+
+//function to allow dragging
+function allowDragging(event){
+  event.preventDefault();
+}
+
+//function that specifies what happens during drag
+function duringDrag(event){
+  event.dataTransfer.setData("text", event.target.id);
+}
+
+//function that handles the drop
+function duringDrop(event){
+  event.preventDefault();
+  var data = event.dataTransfer.getData("text");
+  var imgIndex = data.indexOf('/assets');
+  var imgsrc = data.substring(imgIndex);
+  var clone = document.createElement("img");
+  clone.setAttribute("src", imgsrc);
+  if (event.target.src.includes(imgsrc)){
+    event.target.parentNode.removeChild(event.target);
+    if(imgsrc.includes('chocolate')){
+      updateBank(6);
+    }
+    else if(imgsrc.includes('cookie-dough')){
+      updateBank(6);
+    }
+    else if(imgsrc.includes('vanilla')){
+      updateBank(3);
+    }
+    else if(imgsrc.includes('strawberry')){
+      updateBank(5);
+    }
+    else{
+      updateBank(4);
+    }
+  }
+  else{
+    updateBank(-10);
+  }
+}
+
+function buttonPress(id){
+  var audio = document.getElementById(id);
+  audio.play();
+}
--- server.improved.js
+++ server.improved.js
@@ -1,29 +1,36 @@
-const http = require( 'http' ),
-      fs   = require( 'fs' ),
-      bodyParser = require('body-parser'),
-      // IMPORTANT: you must run `npm install` in the directory for this assignment
-      // to install the mime library used in the following line of code
+const bodyParser = require('body-parser'),
       express = require('express'),
       app = express(),
-      port = process.env.PORT || 3000
+      path = require('path'),
+      port = process.env.PORT || 3000;
 
 const appdata = [
   { 'model': 'toyota', 'year': 1999, 'mpg': 23 },
   { 'model': 'honda', 'year': 2004, 'mpg': 30 },
   { 'model': 'ford', 'year': 1987, 'mpg': 14}
 ]
+var money = 0;
+app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
-app.use("/js", express.static(__dirname + '/public/js'));
-app.use("/css", express.static(__dirname + '/public/css'));
-app.use("/assets", express.static(__dirname + '/public/assets'));
+app.use(express.static('public'));
 app.get('/', function(req, res) {
-  res.sendFile(__dirname + '/public/index.html');
+  res.redirect('/home');
 })
-app.get('/index.html', function(req, res) {
-  res.sendFile(__dirname + '/public/index.html');
+app.get('/home', function(req, res) {
+  var thepath = path.normalize(__dirname + '/public/html/home.html');
+  res.sendFile(thepath);
 })
-app.post('/submit', function(req, res) {
-  res.status(204).send();
+app.get('/instructions', function(req, res) {
+  var thepath = path.normalize(__dirname + '/public/html/instructions.html');
+  res.sendFile(thepath);
+})
+app.get('/game', function(req, res) {
+  var thepath = path.normalize(__dirname + '/public/html/game.html');
+  res.sendFile(thepath);
+})
+app.post('/updateBank', function(req, res) {
+  money += Number(req.body.amount);
+  res.send({result: money});
 })
 
 app.listen(port)
