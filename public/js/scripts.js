f0c63f729de1ae9ccee8c841c6c34d53c01020bf
Ken Desrosiers <kdesrosiers@wpi.edu>
2019-09-05 12:43:56 -0400

    more progress

--- public/js/scripts.js
+++ public/js/scripts.js
@@ -33,12 +33,16 @@ function generateCustomer(){
   })
 }
 
+
+
 //generate a random number
 function random(min, max){
   min = Math.ceil(min);
   max = Math.floor(max);
   return Math.floor(Math.random() * (max - min + 1)) + min;
 }
+
+
 
 //update my money
 function updateBank(num) {
