8d0c2abd873c39583364ac1af3f7c84ce85cf3ce
Ken Desrosiers <kdesrosiers@wpi.edu>
2019-09-04 20:51:45 -0400

    way too much effort, not even worth

--- public/js/scripts.js
+++ public/js/scripts.js
@@ -1,32 +1,36 @@
 //to generate a new customer every so often
 function generateCustomer(){
   var rand = random(1, 4);
-  var newEl = document.createElement('div');
-  var divString = "";
-  //var customer = document.createElement("IMG");
-  var order = document.createElement("P");
-  if (rand === 1) {
-    divString="<div style='display: inline;'><img ondrop='duringDrop(event)' ondragover='allowDragging(event)' style='height:70px; width:50px;' src='../assets/chocolate.png'/></div>";
-    newEl.innerHTML = divString;
-    order.innerHTML = "CHOCOLATE";
+  var date = new Date().getTime();
+  var imgProperties = "";
+  if(rand === 1){
+    imgProperties = "<img ondrop='duringDrop(event)' ondragover='allowDragging(event)' style='height:70px; width:50px;' src='../assets/chocolate.png' class='"+date+"'/>";
+    orderProperties = "<li class='"+date+"'>CHOCOLATE</li>";
   }
-  else if (rand === 2){
-    divString="<div style='display: inline;'><img ondrop='duringDrop(event)' ondragover='allowDragging(event)' style='height:70px; width:50px;' src='../assets/cookie-dough.png'/></div>";
-    newEl.innerHTML = divString;
-    order.innerHTML = "COOKIE DOUGH";
+  else if(rand === 2){
+    imgProperties = "<img ondrop='duringDrop(event)' ondragover='allowDragging(event)' style='height:70px; width:50px;' src='../assets/cookie-dough.png' class='"+date+"'/>";
+    orderProperties = "<li class='"+date+"'>COOKIE DOUGH</li>";
   }
-  else if (rand === 3) {
-    divString="<div style='display: inline;'><img ondrop='duringDrop(event)' ondragover='allowDragging(event)' style='height:70px; width:50px;' src='../assets/vanilla.png'/></div>";
-    newEl.innerHTML = divString;
-    order.innerHTML = "VANILLA";
+  else if(rand === 3){
+    imgProperties = "<img ondrop='duringDrop(event)' ondragover='allowDragging(event)' style='height:70px; width:50px;' src='../assets/vanilla.png' class='"+date+"'/>";
+    orderProperties = "<li class='"+date+"'>VANILLA</li>";
   }
-  else if (rand === 4) {
-    divString="<div style='display: inline;'><img ondrop='duringDrop(event)' ondragover='allowDragging(event)' style='height:70px; width:50px;' src='../assets/strawberry.png'/></div>";
-    newEl.innerHTML = divString;
-    order.innerHTML = "STRAWBERRY";
+  else if(rand === 4){
+    imgProperties = "<img ondrop='duringDrop(event)' ondragover='allowDragging(event)' style='height:70px; width:50px;' src='../assets/strawberry.png' class='"+date+"'/>";
+    orderProperties = "<li class='"+date+"'>STRAWBERRY</li>";
   }
-  document.querySelector("#line").appendChild(newEl.firstChild);
-  document.querySelector('#orders').appendChild(order);
+  const json = {img: imgProperties, order: orderProperties};
+  body = JSON.stringify(json);
+  fetch('/neworder', {
+    method: 'POST',
+    headers: {'Content-Type': 'application/json'},
+    body
+  })
+  .then (response => response.json())
+  .then (response => {
+    document.querySelector('#line').innerHTML = response.images.join("");
+    document.querySelector('#orders').innerHTML = response.orders.join("");
+  })
 }
 
 //generate a random number
@@ -68,6 +72,12 @@ function updateBank(num) {
   }
 }
 
+
+
+
+
+
+//DRAGGING STUFF!!!!!!!
 //function to allow dragging
 function allowDragging(event){
   event.preventDefault();
@@ -81,24 +91,50 @@ function duringDrag(event){
 //function that handles the drop
 function duringDrop(event){
   event.preventDefault();
+  var id = "";
   var data = event.dataTransfer.getData("text");
   var imgIndex = data.indexOf('/assets');
   var imgsrc = data.substring(imgIndex);
   var clone = document.createElement("img");
   clone.setAttribute("src", imgsrc);
+  var newfinished = document.createElement("LI");
   if (event.target.src.includes(imgsrc)){
-    event.target.parentNode.removeChild(event.target);
+    id = event.target.className;
+    const json = { id: id},
+    body = JSON.stringify( json )
+    fetch( '/remove', {
+      method:'POST',
+      headers: {'Content-Type': 'application/json'},
+      body
+    })
+  .then( promiseresponse => promiseresponse.json())
+  .then(response => {
+    document.querySelector('#line').innerHTML = response.images.join("");
+    document.querySelector('#orders').innerHTML = response.orders.join("");
+  })
     if(imgsrc.includes('chocolate')){
       updateBank(6);
+      var textnode = document.createTextNode("CHOCOLATE: +$6");
+      newfinished.appendChild(textnode);
+      document.querySelector('#completed').appendChild(newfinished);
     }
     else if(imgsrc.includes('cookie-dough')){
       updateBank(6);
+      var textnode = document.createTextNode("COOKIE DOUGH: +$6");
+      newfinished.appendChild(textnode);
+      document.querySelector('#completed').appendChild(newfinished);
     }
     else if(imgsrc.includes('vanilla')){
       updateBank(3);
+      var textnode = document.createTextNode("VANILLA: +$3");
+      newfinished.appendChild(textnode);
+      document.querySelector('#completed').appendChild(newfinished);
     }
     else if(imgsrc.includes('strawberry')){
       updateBank(5);
+      var textnode = document.createTextNode("STRAWBERRY: +$5");
+      newfinished.appendChild(textnode);
+      document.querySelector('#completed').appendChild(newfinished);
     }
     else{
       updateBank(4);
@@ -106,5 +142,8 @@ function duringDrop(event){
   }
   else{
     updateBank(-10);
+    var textnode = document.createTextNode("WRONG KIND!: -$10");
+    newfinished.appendChild(textnode);
+    document.querySelector('#completed').appendChild(newfinished);
   }
 }
--- server.improved.js
+++ server.improved.js
@@ -4,12 +4,9 @@ const bodyParser = require('body-parser'),
       path = require('path'),
       port = process.env.PORT || 3000;
 
-const appdata = [
-  { 'model': 'toyota', 'year': 1999, 'mpg': 23 },
-  { 'model': 'honda', 'year': 2004, 'mpg': 30 },
-  { 'model': 'ford', 'year': 1987, 'mpg': 14}
-]
 var money = 0;
+var imageOrders = [];
+var orders = [];
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static('public'));
@@ -34,7 +31,30 @@ app.post('/updateBank', function(req, res) {
 })
 app.post('/reset', function(req, res) {
   money = Number(req.body.amount);
+  imageOrders = [];
+  orders = [];
   res.send({result: money});
+})
+app.post('/neworder', function(req, res) {
+  imageOrders.push(req.body.img);
+  orders.push(req.body.order);
+  res.send({images: imageOrders, orders: orders});
+})
+app.post('/remove', function(req, res) {
+  var id = req.body.id;
+  imageOrders.forEach(function(element) {
+    if(element.includes(id)){
+      var index = imageOrders.indexOf(element);
+      imageOrders.splice(index, 1);
+    }
+  });
+  orders.forEach(function(element) {
+    if(element.includes(id)){
+      var index = orders.indexOf(element);
+      orders.splice(index, 1);
+    }
+  });
+  res.send({images: imageOrders, orders: orders});
 })
 
 app.listen(port)
