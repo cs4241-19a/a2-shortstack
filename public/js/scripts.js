a0d62747864691d89cafcf5bb247425ca77dd2eb
nbloniarz <nbloniarz@wpi.edu>
2019-09-06 17:15:03 +0000

    📻🚒 Checkpoint ./public/index.html:6266250/6256 ./public/css/style.css:6266250/615 ./public/js/scripts.js:6266250/568

--- public/js/scripts.js
+++ public/js/scripts.js
@@ -1,3 +1,16 @@
 // Add some Javascript code here, to run on the front end.
 
-console.log("Welcome to assignment 2!")
+ //FUNCTION TO CONROL Hide-Show of ADD NEW FORM
+    function displayNewForm(){
+      var form = document.getElementById("newForm");
+      console.log(form.style.display)
+      if(form.style.display === "none"){
+        form.style.diplay = "block";
+        console.log("CHANGED" + form.style.display)
+
+      }
+      else{
+        console.log("HIDE")
+        form.style.display = "none";
+      }
+    }
