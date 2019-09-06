0f453eeba25808cbee7b124c16d486e3b33cb1bf
James Plante <jplante@wpi.edu>
2019-09-06 16:08:27 -0400

    Created separate script file

--- public/js/scripts.js
+++ public/js/scripts.js
@@ -1,3 +1,53 @@
-// Add some Javascript code here, to run on the front end.
 
-console.log("Welcome to assignment 2!")
+"use strict"
+
+let counter = 0;
+
+const incrementCounter = function(e) {
+  const currentLOC = document.getElementById('counter');
+  counter = Number(currentLOC.innerHTML) + 1
+  currentLOC.innerHTML = counter;
+}
+
+const submit = function (e, action) {
+  // prevent default form action from being carried out
+  e.preventDefault()
+
+  const cursors = document.querySelector('#cursors'),
+    hobbyists = document.querySelector('#hobbyists'),
+    csMajors = document.querySelector('#cs_maj'),
+    softEng = document.querySelector('#soft_eng')
+    server = document.querySelector('#server_farm'),
+    quantum = document.querySelector('#quantum'),
+    currentLOC = document.getElementById('counter'),
+    json = { 
+      action: action,
+      cursors: cursors.value,
+      hobbyists: hobbyists.value,
+      csMajors: csMajors.value,
+      softEng: softEng.value,
+      server: server.value,
+      quantum: quantum.value,
+      currentLOC: currentLOC.innerHTML},
+    body = JSON.stringify(json)
+
+  fetch('/submit', {
+    method: 'POST',
+    body
+  })
+    .then(function (response) {
+      // do something with the reponse 
+      console.log(response)
+    })
+
+  return false
+}
+
+window.onload = function () {
+  const button = document.getElementById('purchase')
+  button.onclick = submit
+
+  const incButton = document.getElementById('clicker')
+  incButton.onclick = incrementCounter
+}
+
