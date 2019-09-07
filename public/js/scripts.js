0d3344e4665304a4cf43a83095c2f15ca388f16a
nbloniarz <nbloniarz@wpi.edu>
2019-09-07 00:45:00 +0000

    🏇🏁 Checkpoint ./public/js/scripts.js:6266250/1140 ./public/index.html:6266250/2514

--- public/js/scripts.js
+++ public/js/scripts.js
@@ -2,22 +2,28 @@
 
  //Function to control hide-show of ADD NEW FORM
     function displayNewForm(){
-      var form = document.getElementById("newForm");
-      console.log(form.style.visible)
-      if(form.style.visibility === 'visible'){
-        form.style.visibility = 'hidden';
-        console.log("CHANGED " + form.style.display)
-
+      var newForm = document.getElementById("newForm");
+      var modForm = document.getElementById("modForm");
+      modForm.style.display = 'none'
+      if(newForm.style.display === 'none'){
+        newForm.style.display = 'inline';
       }
       else{
-        console.log("HIDE")
-        form.style.visibility = 'visible';
+        newForm.style.display = 'none';
       }
     }
  
 //Function to control hide-show of MOD DATA FORM
   function displayModifyForm(){
-  
+    var newForm = document.getElementById("newForm");
+    newForm.style.display = 'none'
+    var modForm = document.getElementById("modForm");
+    if(modForm.style.display === 'none'){
+        modForm.style.display = 'inline';
+      }
+      else{
+        modForm.style.display = 'none';
+      }
   }
 
 //Function to control hide-how of entire database table
