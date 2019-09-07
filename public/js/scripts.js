1f2096c9efb3e70f95b0182f1308c37501eb9ee0
Peter Christakos <newtownfam@gmail.com>
2019-09-06 20:22:15 -0400

    Added basic table using tabulator

--- public/js/scripts.js
+++ public/js/scripts.js
@@ -1,16 +1,19 @@
 // Add some Javascript code here, to run on the front end.
 
 console.log("Welcome to assignment 2!")
+//var Tabulator = require('tabulator-tables');
 
 // array to hold all inputs
-var input = []
+var tableData = []
 
 const submit = function( e ) {
     // prevent default form action from being carried out
     e.preventDefault()
 
-    const input = document.querySelector( '#yourname' ),
-          json = { yourname: input.value },
+    const amt = document.querySelector( '#amount' ),
+          cat = document.querySelector( '#category' ),
+          mon = document.querySelector( '#month' ),
+          json = { amount: amt.value, category: cat.value, month: mon.value },
           body = JSON.stringify( json )
 
     fetch( '/submit', {
@@ -20,21 +23,44 @@ const submit = function( e ) {
     .then( function( response ) {
       // do something with the reponse 
       console.log( response )
-      // append to array of inputs
-      console.log( "amount of responses: " + input.length)
+      // append to array of input
+      tableData.push(json)
+      console.log(tableData.length)
+      table.setData(tableData);
     })
 
     return false
-  }
+}
+// submit button saves data
+window.onload = function() {
+const button = document.getElementById( 'button' )
+button.onclick = submit
+}
 
-  window.onload = function() {
-    const button = document.querySelector( 'button' )
-    button.onclick = submit
-  }
-
-
+// hide $ on amount
 var amount = document.getElementById("amount")
 amount.addEventListener("click", function hide() {
-    amount.value = "";
+amount.value = "";
 })
 
+// make table
+var table = new Tabulator("#expense-table", {
+    data:tableData,           //load row data from array
+	layout:"fitColumns",      //fit columns to width of table
+	responsiveLayout:"hide",  //hide columns that dont fit on the table
+	tooltips:true,            //show tool tips on cells
+	addRowPos:"top",          //when adding a new row, add it to the top of the table
+	history:true,             //allow undo and redo actions on the table
+	pagination:"local",       //paginate the data
+	paginationSize:7,         //allow 7 rows per page of data
+	movableColumns:true,      //allow column order to be changed
+	resizableRows:true,       //allow row order to be changed
+	initialSort:[             //set the initial sort order of the data
+		{column:"name", dir:"asc"},
+	],
+    columns:[                  //define the table columns
+        {title:"Amount", field:"amount", editor:"input"},
+		{title:"Category", field:"category", align:"left", editor:true, editorParams:{values:["Merchandise", "Restaurants", "Gasoline", "Travel/Ent", "Supermarkets"]}},
+		{title:"Month", field:"month", width:95, editor:"select", editorParams:{values:["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sept", "Oct", "Nov", "Dec"]}}
+	],
+});
