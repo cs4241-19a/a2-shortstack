34a6be7321768509f5922f1fab795079ab763560
manasmehta18 <manasmehta.18@gmail.com>
2019-09-07 00:43:41 -0400

    added logout button, boardname and username to task.html on the navbar

--- public/js/formTask.js
+++ public/js/formTask.js
@@ -1,3 +1,8 @@
+const back = function( e ) {
+    window.location = "/"
+    return false
+}
+
 window.onload = function() {
     fetch( '/receive')
         .then( function( response ) {
@@ -8,7 +13,12 @@ window.onload = function() {
         var b = localStorage.getItem('myBoard')
         console.log(n)
         console.log(response)
-        document.getElementById('name').style.backgroundColor = response.users[n][b].color;
+        document.getElementById('nav').style.backgroundColor = response.users[n][b].color;
+        document.getElementById('bName').innerText = JSON.stringify(response.users[n][b].boardName).replace(/^"(.*)"$/, '$1');
+        document.getElementById('usrTxt').innerText = JSON.stringify(response.users[n][b].username).replace(/^"(.*)"$/, '$1');
         console.log( "get response: ", response )
     })
-    };
+
+    const button = document.querySelector( 'button' )
+    button.onclick = back
+};
