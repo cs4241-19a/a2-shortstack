cd98b0ba20afac8ce876a82bd267eced9d819b44
nbloniarz <nbloniarz@wpi.edu>
2019-09-07 18:35:11 +0000

    🐓🗻 Checkpoint ./public/js/scripts.js:6266250/21597

--- public/js/scripts.js
+++ public/js/scripts.js
@@ -99,18 +99,9 @@ function showEditDataForm(){
           html += "<input type=\"text\" class=\"lName\" value=\""
           html += allData[selectedIndex].lName + "\"><br>"
           var month = allData[selectedIndex].month
-          html += monthNameToNum(month)
+          html += monthToHTML(month)
           var day = allData[selectedIndex].day
           html += daysToHTML(month, day)
-          
-          /*
-           
-        <br>
-        <label for="days">Day of Birth</label>
-        <select name="days" class="days">  
-        </select>
-        <br>
-          */
           html += "</form>"
           document.getElementById("Container").innerHTML = ""
           document.getElementById("Container").innerHTML = html
@@ -278,8 +269,8 @@ const DateDataForDropdown = {
 }
 
 
-//**
-function monthNameToNum(month){
+//******* UTILITY DATE FUNCTIONS *******//
+function monthToHTML(month){
   let html = ""
   switch(month){
     case "January":
@@ -297,7 +288,6 @@ function monthNameToNum(month){
       html += "<option value=\"November\">Novemeber</option>"
       html += "<option value=\"December\">December</option></select><br>"
       return html;
-      break;
     case "February":
       html += "<select name=\"month\" class=\"month\">"
       html += "<option value=\"January\">January</option>"
@@ -313,7 +303,6 @@ function monthNameToNum(month){
       html += "<option value=\"November\">Novemeber</option>"
       html += "<option value=\"December\">December</option></select><br>"
       return html;
-      break;
     case "March":
       html += "<select name=\"month\" class=\"month\">"
       html += "<option value=\"January\">January</option>"
@@ -329,7 +318,6 @@ function monthNameToNum(month){
       html += "<option value=\"November\">Novemeber</option>"
       html += "<option value=\"December\">December</option></select><br>"
       return html;
-      break;
     case "April":
       html += "<select name=\"month\" class=\"month\">"
       html += "<option value=\"January\">January</option>"
@@ -345,7 +333,6 @@ function monthNameToNum(month){
       html += "<option value=\"November\">Novemeber</option>"
       html += "<option value=\"December\">December</option></select><br>"
       return html;
-      break;
     case "May":
       html += "<select name=\"month\" class=\"month\">"
       html += "<option value=\"January\">January</option>"
@@ -361,7 +348,6 @@ function monthNameToNum(month){
       html += "<option value=\"November\">Novemeber</option>"
       html += "<option value=\"December\">December</option></select><br>"
       return html;
-      break;
     case "June":
       html += "<select name=\"month\" class=\"month\">"
       html += "<option value=\"January\">January</option>"
@@ -377,7 +363,6 @@ function monthNameToNum(month){
       html += "<option value=\"November\">Novemeber</option>"
       html += "<option value=\"December\">December</option></select><br>"
       return html;
-      break;
     case "July":
       html += "<select name=\"month\" class=\"month\">"
       html += "<option value=\"January\">January</option>"
@@ -393,7 +378,6 @@ function monthNameToNum(month){
       html += "<option value=\"November\">Novemeber</option>"
       html += "<option value=\"December\">December</option></select><br>"
       return html;
-      break;
     case "August":
       html += "<select name=\"month\" class=\"month\">"
       html += "<option value=\"January\">January</option>"
@@ -409,7 +393,6 @@ function monthNameToNum(month){
       html += "<option value=\"November\">Novemeber</option>"
       html += "<option value=\"December\">December</option></select><br>"
       return html;
-      break;
     case "September":
       html += "<select name=\"month\" class=\"month\">"
       html += "<option value=\"January\">January</option>"
@@ -425,7 +408,6 @@ function monthNameToNum(month){
       html += "<option value=\"November\">Novemeber</option>"
       html += "<option value=\"December\">December</option></select><br>"
       return html;
-      break;
     case "October":
       html += "<select name=\"month\" class=\"month\">"
       html += "<option value=\"January\">January</option>"
@@ -441,7 +423,6 @@ function monthNameToNum(month){
       html += "<option value=\"November\">Novemeber</option>"
       html += "<option value=\"December\">December</option></select><br>"
       return html;
-      break;
     case "November":
       html += "<select name=\"month\" class=\"month\">"
       html += "<option value=\"January\">January</option>"
@@ -457,7 +438,6 @@ function monthNameToNum(month){
       html += "<option selected=\"selected\" value=\"November\">Novemeber</option>"
       html += "<option value=\"December\">December</option></select><br>"
       return html;
-      break;
     case "December":
       html += "<select name=\"month\" class=\"month\">"
       html += "<option value=\"January\">January</option>"
@@ -473,6 +453,75 @@ function monthNameToNum(month){
       html += "<option value=\"November\">Novemeber</option>"
       html += "<option selected=\"selected\" value=\"December\">December</option></select><br>"
       return html;
-      break;
   }
+}
+
+function daysToHTML(month, day){
+  let totalDays = hasDays(monthToNum(month));
+  let html = "<label for=\"days\">Day of Birth</label>"
+  html += "<select name=\"days\" class=\"days\">"
+  for(let i = 1; i <= totalDays; i++){
+    html += "<option "
+    if
+  }
+  html += "</select><br>"
+}
+
+function monthToNum(month){
+  switch(month){
+    case "January":
+      return 0;
+    case "February":
+      return 1;
+    case "March":
+      return 2;
+    case "April":
+      return 3;
+    case "May":
+      return 4;
+    case "June":
+      return 5;
+    case "July":
+      return 6;
+    case "August":
+      return 7;
+    case "September":
+      return 8;
+    case "October":
+      return 9;
+    case "November":
+      return 10;
+    case "December":
+      return 11;
+  }
+}
+
+function hasDays(month){
+  switch(month){
+    case 0:
+      return 31;
+    case 1:
+      return 29;
+    case 2:
+      return 31;
+    case 3:
+      return 30;
+    case 4:
+      return 31;
+    case 5:
+      return 30;
+    case 6:
+      return 31;
+    case 7:
+      return 31;
+    case 8:
+      return 30;
+      break;
+    case 9:
+      return 31;
+    case 10:
+      return 30;
+    case 11:
+      return 31;
+\  }
 }
