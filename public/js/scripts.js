1123bfbf91df127f59cd204265bc6200c3968cd7
Glitch (a2-benemrick) <none>
2019-09-08 21:35:07 +0000

    🍃🍟 Checkpoint ./public/index.html:2480412/545 ./public/css/style.css:2480412/837 ./public/js/scripts.js:2480412/91

--- public/js/scripts.js
+++ public/js/scripts.js
@@ -1,3 +1,8 @@
 // Add some Javascript code here, to run on the front end.
 
+unsplash.search.photos("dogs", 1)
+  .then(toJson)
+  .then(json => {
+    // Your code
+  });
 console.log("Welcome to assignment 2!")
