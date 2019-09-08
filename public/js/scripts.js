f6964d2f56366943713c08fe044d4bfeabb71f3b
Glitch (paristhecity-a2-pllopez) <none>
2019-09-08 21:25:31 +0000

    🌄📮 Checkpoint ./public/js/scripts.js:6266626/381 ./public/index.html:6266626/107

--- public/js/scripts.js
+++ public/js/scripts.js
@@ -8,7 +8,11 @@ const days = [
 //.boardsection[data-board]
 //const nextButton = document.querySelector( '#next' );
 
-const nextAction = function() {
-  document.querySelector( '.boardsection' ).css();
+const nextAction = function(num) {
+  var day = document.querySelector( '.boardsection[data-board=day_' + num + ']' );
+  var body = document.querySelector( ':not(.boardsection[data-board=day_' + num + '])' );
+  day.animate('next', '1s', 'forwards');
+  body.animate('nextbody', '1s', 'forwards');
+  
 }
 
