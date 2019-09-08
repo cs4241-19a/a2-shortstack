5c294cef80f7fd55a51eef4f4cb73c2a8bf63992
Glitch (paristhecity-a2-pllopez) <none>
2019-09-08 20:13:20 +0000

    💶🌂 Checkpoint ./public/css/style.css:6266626/324 ./public/index.html:6266626/149 ./public/js/scripts.js:6266626/459

--- public/js/scripts.js
+++ public/js/scripts.js
@@ -1,3 +1,10 @@
 // Add some Javascript code here, to run on the front end.
 
 console.log("Welcome to assignment 2!")
+
+const days = [
+  "Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"
+]
+//.boardsection[data-board]
+const nextButton = document.querySelector( '' );
+console.log(nextButton)
