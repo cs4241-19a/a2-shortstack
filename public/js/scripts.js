174ecb3c137bc6714071bad5384f3fc9dad8a73e
Glitch (winnycheng-a2-shortstack) <none>
2019-09-08 03:16:55 +0000

    🎲😻 Checkpoint ./public/index.html:1011354/628 ./public/js/scripts.js:1011354/1960

--- public/js/scripts.js
+++ public/js/scripts.js
@@ -1,3 +1,24 @@
-// Add some Javascript code here, to run on the front end.
+const submit = function( e ) {
+  // prevent default form action from being carried out
+  e.preventDefault()
 
-console.log("Welcome to assignment 2!")
+  const input = document.querySelector( '#yourname' ),
+        json = { yourname: input.value },
+        body = JSON.stringify( json )
+
+  fetch( '/submit', {
+    method:'POST',
+    body 
+  })
+  .then( function( response ) {
+    // do something with the reponse 
+    console.log( response )
+  })
+
+  return false
+}
+
+window.onload = function() {
+  const button = document.querySelector( 'button' )
+  button.onclick = submit
+}
