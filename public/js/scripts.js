fc51309976e820b777e8aadf17a11e99d1f8d5a2
eos7l <31972892+eos7l@users.noreply.github.com>
2019-09-07 22:51:50 -0400

    prototype

--- public/js/scripts.js
+++ public/js/scripts.js
@@ -1,3 +1,11 @@
 // Add some Javascript code here, to run on the front end.
 
-console.log("Welcome to assignment 2!")
+// PRELOADING SCREEN
+jQuery('a:not([target="_blank"]):not([href*=#]):not([href^=mailto]):not(.fancybox-media):not(.btn.responsive-menu):not(a[href$="jpg"]):not([href$="jpeg"]):not(a[href$="gif"]):not(a[href$="png"]):not(a.ajax-link)').on('click', function(){
+    var href = jQuery(this).attr('href');
+    jQuery('.preloader').fadeIn(300);
+    setTimeout(function(){
+        window.location = href;
+    }, 300);
+    return false;
+});
--- server.improved.js
+++ server.improved.js
@@ -1,72 +1,129 @@
+var admin = require('firebase-admin');
+var serviceAccount=require("./serviceKey.json")
+
+admin.initializeApp({
+  credential:admin.credential.cert(serviceAccount),
+      databaseURL:'https://cs4241-a2.firebaseio.com'
+});
+var db = admin.database();
+var usersRef = db.ref('/');
+
 const http = require( 'http' ),
       fs   = require( 'fs' ),
-      // IMPORTANT: you must run `npm install` in the directory for this assignment
-      // to install the mime library used in the following line of code
       mime = require( 'mime' ),
       dir  = 'public/',
-      port = 3000
+      port = 8000;
 
-const appdata = [
-  { 'model': 'toyota', 'year': 1999, 'mpg': 23 },
-  { 'model': 'honda', 'year': 2004, 'mpg': 30 },
-  { 'model': 'ford', 'year': 1987, 'mpg': 14} 
+const appdata = [ //can add/edit/ delete any object in here
+  { 'currentGrade': 'toyota', 'desired': 1999, 'finalWorth': 23 },
+  { 'currentGrade': 'honda', 'desired': 2004, 'finalWorth': 30 },
+  { 'currentGrade': 'ford', 'desired': 1987, 'finalWorth': 14 }
 ]
+
 
 const server = http.createServer( function( request,response ) {
   if( request.method === 'GET' ) {
-    handleGet( request, response )    
-  }else if( request.method === 'POST' ){
-    handlePost( request, response ) 
+    handleGet( request, response )
+  }else if( request.method === 'POST' ){ //could add more functions like delete here, but could also have just POST and have the urls to determine what to do
+    handlePost( request, response )
   }
-})
+});
 
+//use handleGet to display data structure (server) in UI (server to UI)
 const handleGet = function( request, response ) {
-  const filename = dir + request.url.slice( 1 ) 
-
+  const filename = dir + request.url.slice( 1 )
   if( request.url === '/' ) {
-    sendFile( response, 'public/index.html' )
-  }else{
+    sendFile( response, 'public/index.html' ) //do sendFile for javascript file
+  }else if(request.url === '/getdata'){
+    sendData(response, appdata)
+  }else if(request.url==='/receive'){
+    usersRef.on('value', function(snapshot) {
+      console.log(snapshot.val());
+      response.end(JSON.stringify(snapshot.val()))
+    }, function (errorObject) {
+      console.log('The read failed: ' + errorObject.code);
+    });
+  }
+
+  else{
     sendFile( response, filename )
   }
-}
-
+};
+//communicate from HTML to server
+//change url to look at specific file (same as a1 with switch statement) (if request.url = delete, delete the data)
 const handlePost = function( request, response ) {
-  let dataString = ''
-
+  let dataString = '';
   request.on( 'data', function( data ) {
-      dataString += data 
-  })
-
+      dataString += data
+  });
   request.on( 'end', function() {
-    console.log( JSON.parse( dataString ) )
+    const data = JSON.parse(dataString);
+    writeUserData(data.currentGrade, data.currentGrade, data.desired);
+      //For validation
+/*    if( request.url === '/add') {
+      let foundName=false //figuring out if something already exists
+      for (let i=0; i<appdata.length; i++){
+       if(appdata[i],name === data.name) foundName = true
+      }
+      if(foundName == false){
+       appdata.push(data)
+      }
+  }*/
 
-    // ... do something with the data here!!!
-
-    response.writeHead( 200, "OK", {'Content-Type': 'text/plain' })
-    response.end()
+    switch( request.url ) {
+      case '/submit':
+      //server logic
+        let totalGallons = (parseInt(data.tripDistance) / parseInt(data.finalWorth))
+        let totalCost = (totalGallons*(parseInt(data.gasPrice)))
+        const carData = {
+          'currentGrade': data.currentGrade,
+          'desired': data.desired,
+          'finalWorth': data.finalWorth,
+        };
+        console.log(carData);
+        appdata.push( carData );
+        response.writeHead( 200, "OK", {'Content-Type': 'text/plain' })
+        response.end();
+        break;
+      case '/delete':
+        appdata.delete( data );
+        break;
+      case '/update':
+        appdata.update( data );
+        break;
+      default:
+        response.end( '404 Error: File Not Found' )
+  }
   })
-}
+};
+
+const sendData = function(response, carsdata){
+  const type = mime.getType( carsdata ) ;
+  response.writeHeader( 200, { 'Content-Type': type });
+  response.write(JSON.stringify({data: carsdata}));
+  response.end();
+};
 
 const sendFile = function( response, filename ) {
-   const type = mime.getType( filename ) 
+   const type = mime.getType( filename );
 
    fs.readFile( filename, function( err, content ) {
-
-     // if the error = null, then we've loaded the file successfully
      if( err === null ) {
-
-       // status code: https://httpstatuses.com
-       response.writeHeader( 200, { 'Content-Type': type })
+       response.writeHeader( 200, { 'Content-Type': type });
        response.end( content )
-
      }else{
-
-       // file not found, error code 404
-       response.writeHeader( 404 )
+       response.writeHeader( 404 );
        response.end( '404 Error: File Not Found' )
-
      }
    })
+};
+
+function writeUserData(ref, currentGrade, desired) {
+  var usernameRef = usersRef.child(ref);
+  usernameRef.set({
+    currentGrade: currentGrade,
+    desired: desired,
+  });
 }
 
-server.listen( process.env.PORT || port )
+server.listen( process.env.PORT || port );
