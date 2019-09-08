860ae0d4df111a166adf8c70531c914a479b3d8f
Glitch (winnycheng-a2-shortstack) <none>
2019-09-08 04:04:57 +0000

    🎻🏊🏼 Checkpoint ./public/index.html:1011354/88 ./public/js/scripts.js:1011354/795

--- public/js/scripts.js
+++ public/js/scripts.js
@@ -1,7 +1,36 @@
-const submit = function( e ) {
+// const submit = function( e ) {
+//   // prevent default form action from being carried out
+//   e.preventDefault()
+
+//   const input = document.querySelector( '#yourname' ),
+//         json = { yourname: input.value },
+//         body = JSON.stringify( json )
+
+//   fetch( '/submit', {
+//     method:'POST',
+//     body 
+//   })
+//   .then( function( response ) {
+//     // do something with the reponse 
+//     console.log( response )
+//   })
+
+//   return false
+// }
+
+// window.onload = function() {
+//   const button = document.querySelector( 'button' )
+//   button.onclick = submit
+// }
+
+const generate = function(e) {
   // prevent default form action from being carried out
   e.preventDefault()
 
+  const input = {
+    vertices: document.getElementById("veritces").value
+    drawType: document.getElementById()
+  }
   const input = document.querySelector( '#yourname' ),
         json = { yourname: input.value },
         body = JSON.stringify( json )
@@ -16,9 +45,4 @@ const submit = function( e ) {
   })
 
   return false
-}
-
-window.onload = function() {
-  const button = document.querySelector( 'button' )
-  button.onclick = submit
 }
