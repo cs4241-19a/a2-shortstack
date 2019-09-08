2b581da8562506bdfdf4ec282bd0394fe19c2eb8
Glitch (paristhecity-a2-pllopez) <none>
2019-09-08 21:48:34 +0000

    🗃😄 Checkpoint ./public/js/scripts.js:6266626/700 ./public/css/style.css:6266626/96 ./public/index.html:6266626/276

--- public/js/scripts.js
+++ public/js/scripts.js
@@ -4,24 +4,24 @@ console.log("Welcome to assignment 2!")
 
 const days = [
   "Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"
-]
+];
 //.boardsection[data-board]
 //const nextButton = document.querySelector( '#next' );
 
-const nextAction = function(num) {
-  alert("Hello")
-  var day = document.querySelector( '.boardsection[data-board=day_' + num + ']' );
-  var body = document.querySelector( ':not(.boardsection[data-board=day_' + num + '])' );
-  day.animate([
-    
-  ],{
-    duration: 1,
-    
-  }, 
-              'forwards');
-  body.animate('nextbody', '1s', 'forwards');
+const nextAction = function() {
+  var num = 0;
+  var day = document.getElementById( '.boardsection[data-board=day_' + num + ']' );
+  var body = document.getElementById( ':not(.boardsection[data-board=day_' + num + '])' );
+  
+  day.style.color = "blue";
+  
+  //day.style.animation = "next 1s forwards";
+  //body.css({animation: "nextbody 1s forwards"});
   
 }
+          
+//animation: nextbody 1s forwards;
+//animation: next 1s forwards;
 
 
 /*
