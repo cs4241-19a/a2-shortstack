e3edf7d1ba91be367d0d7d25e44d50dd23dba215
Glitch (a2-ktrose) <none>
2019-09-08 07:26:03 +0000

    ⛹🏪 Checkpoint ./public/js/scripts.js:6287718/216

--- public/js/scripts.js
+++ public/js/scripts.js
@@ -17,9 +17,6 @@
         for (let i = 0; i < books.length; i++) {
           const book = books[i];
           const stringBook = JSON.stringify(books[i]);
-          if (book.rating != "canNotRate"){
-              book.status = "completed"
-          }
           let newRow = '<tr>\n';
           newRow += (`<td> ${book.bookName} </td>\n`);
           newRow += (`<td> ${book.authorName} </td>\n`);
@@ -43,7 +40,7 @@
         bookName: document.getElementById( 'bookName' ).value,
         authorName: document.getElementById( 'authorName' ).value,
         comments:document.getElementById( 'comments' ).value,
-        rating: document.querySelector('input[name="rating"]:checked').value,
+        rating: document.getElementById( 'rating' ).value,
         status: 'inprogress'
       };
     
@@ -67,7 +64,7 @@
       document.getElementById( 'bookName' ).value = '';
       document.getElementById( 'authorName' ).value = '';
       document.getElementById('comments').value = '';
-      document.getElementById('rating').checked = false;
+      document.getElementById('rating').value = '';
     };
     
     //validate an order before sending it
