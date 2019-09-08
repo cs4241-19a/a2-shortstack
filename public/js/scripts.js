91cf707260e7006b537e61d3ffceb6efbe9a4089
Glitch (paristhecity-a2-pllopez) <none>
2019-09-08 23:23:29 +0000

    ⛄️🙍 Checkpoint ./public/js/scripts.js:6266626/2223 ./public/css/style.css:6266626/527

--- public/js/scripts.js
+++ public/js/scripts.js
@@ -1,66 +1,12 @@
-// Add some Javascript code here, to run on the front end.
-/*
 console.log("Welcome to assignment 2!")
 
-const days = [
-  "Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"
-];
-*/
 
 $(document).ready(function () {
-  $("#next").on('click', function() {
-    //$(".boardsection[data-board]").css({animation: "nextbody 1s forwards"});
+  $(document).on('click', "#next", function() {
     var sections = $('.boardsection[data-board]');
-    sections.first().insertAfter(sections.last());
-    $(".boardsection[data-board]:first-child").css({animation: "next 1s"});
-    $(".boardsection[data-board]:nth-child(2)").css({animation: "next 1s"});
-    $(".boardsection[data-board]:nth-child(3)").css({animation: "next 1s"});
-    $(".boardsection[data-board]:nth-child(4)").css({animation: "next 1s"});
-    $(".boardsection[data-board]:nth-child(5)").css({animation: "next 1s"});
-    $(".boardsection[data-board]:nth-child(6)").css({animation: "next 1s"});
-    $(".boardsection[data-board]:last-child").css({animation: "next 1s"});
+    $(".boardsection[data-board]:first-child").css({animation: "next 1s forwards"});
 
-    //$(".boardsection[data-board='day_1']").css({
-    //  'order': "1"
-    //});
-  
+    sections.first().insertAfter(sections.last());
+    $(".boardsection[data-board]").css({animation: "next 1s forwards"});
   })
 })
-
-
-// /$(".boardsection[data-board]").style.order + 1 > 7 ? 7 : $(".boardsection[data-board]").style.order + 1
-
-
-
-//.boardsection[data-board]
-//const nextButton = document.querySelector( '#next' );
-/*
-const nextAction = function() {
-  var num = 0;
-  var day = document.getElementById( '.boardsection[data-board=day_' + num + ']' );
-  var body = document.getElementById( ':not(.boardsection[data-board=day_' + num + '])' );
-
-  //day.style.color = "blue";
-  body.css({animation: "nextbody 1s forwards"})
-*/
-  //day.style.animation = "next 1s forwards";
-  //body.css({animation: "nextbody 1s forwards"});
-  
-
-          
-//animation: nextbody 1s forwards;
-//animation: next 1s forwards;
-
-
-/*
-
-document.getElementById("tunnel").animate([
-  // keyframes
-  { transform: 'translateY(0px)' }, 
-  { transform: 'translateY(-300px)' }
-], { 
-  // timing options
-  duration: 1000,
-  iterations: Infinity
-})
-*/
