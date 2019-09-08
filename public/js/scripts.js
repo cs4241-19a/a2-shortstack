c8f7897e296d9931b502bce20ba586f85bf23f6d
Glitch (a2-kreiselman) <none>
2019-09-08 23:01:31 +0000

    🍴🚗 Checkpoint ./public/js/scripts.js:6269127/2222 ./public/index.html:6269127/4302 ./package.json:6269127/482 ./public/css/style.css:6269127/48

--- public/js/scripts.js
+++ public/js/scripts.js
@@ -1,3 +1,94 @@
-// Add some Javascript code here, to run on the front end.
+function submit(  ) {
+    
+    console.log("Testing for name " + document.getElementById('yourname') )
+    document.getElementById('info').submit()
+    setTimeout(function(){
+      clearTable()
+      getData()
+    },1000)
+    
 
-console.log("Welcome to assignment 2!")
+    
+
+    return false
+  }
+  
+  function onDelete(){
+    document.getElementById('delete').submit()
+    setTimeout(function(){
+      clearTable()
+      document.getElementById("dataTable").deleteRow(1)
+      getData()
+    },1000)
+    
+    return false
+  }
+
+ 
+    
+    /*const myDelete = function( e ) {
+    // prevent default form action from being carried out
+    e.preventDefault()
+
+    const deleteName = document.querySelector( '#delname' ),
+          name = deleteName.value
+    
+
+    fetch( '/Ondelete', {
+      method:'POST',
+      name 
+    })
+    .then( function( response ) {
+      // do something with the reponse 
+      alert(name)
+      console.log( response )
+      
+    })
+
+    return false
+  }*/
+    
+    
+    
+    function clearTable()
+    {
+      //$('#dataTable:not(:first)').empty()
+      $.get('/data',function(response){
+        
+        response.forEach(function(item){
+          console.log("cleariunug list" )
+          document.getElementById("dataTable").deleteRow(1)
+        })
+        
+      })
+      /*for(let i=1; i < document.getElementById("dataTable").rows.length; i++)
+        {
+          document.getElementById("dataTable").deleteRow(i)
+        }*/
+      //document.getElementById("dataTable").deleteRow(1)
+      console.log("Table has been reset")
+    }
+    
+    function getData()
+    {
+      $.get('/data',function(response){
+        //clearTable(document.getElementById("dataTable"))
+        //clearTable()
+        
+       // setTimeout(function(){
+          
+          response.forEach(function(item){
+          console.log("item is " +item)
+          $('#dataTable').append('<tr><td>' + item.name + '</td><td>' + item.year + '</td><td>' + item.inches  + '</td><td>' + item.cm + '</td></tr>')
+          console.log("wrote " + item.name)
+        })
+          
+        //},2000)
+        
+      })
+      console.log("Done writing")
+    }
+    
+    
+  
+    
