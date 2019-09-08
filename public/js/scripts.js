de0c95e556ea87ab62e434a880c560ed51a76205
Glitch (a2-benemrick) <none>
2019-09-08 21:47:06 +0000

    🕵🌍 Checkpoint ./public/js/scripts.js:2480412/702 ./public/index.html:2480412/440

--- public/js/scripts.js
+++ public/js/scripts.js
@@ -1,8 +1,19 @@
-// Add some Javascript code here, to run on the front end.
+// ES Modules syntax
+import Unsplash from 'unsplash-js';
 
-unsplash.search.photos("dogs", 1)
+// require syntax
+const Unsplash = require('unsplash-js').default;
+
+const unsplash = new Unsplash({
+  applicationId: "210c371817cd6adae5a2fd6484fb09a9c92f0fec9d3443744a797518224071ef",
+  secret: "fd62ab61acfdb6f6a1a725f41856dff34770e89a875c51224515c1c2aca625da"
+});
+
+const randPhoto = function() {
+  unsplash.search.photos("dogs", 1)
   .then(toJson)
   .then(json => {
     // Your code
   });
-console.log("Welcome to assignment 2!")
+}
+
