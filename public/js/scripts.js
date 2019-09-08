b46325f7ae346248986e52b00080b2bbb0746ed8
Glitch (paristhecity-a2-pllopez) <none>
2019-09-08 22:00:31 +0000

    🏂👆 Checkpoint ./public/js/scripts.js:6266626/662

--- public/js/scripts.js
+++ public/js/scripts.js
@@ -1,24 +1,38 @@
 // Add some Javascript code here, to run on the front end.
-
+/*
 console.log("Welcome to assignment 2!")
 
 const days = [
   "Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"
 ];
+*/
+
+  
+  $('#next').on('click', function() {
+    alert("Hello");
+    $(".boardsection[data-board='day_1']").css({animation: "body 1s forwards"});
+    //$(':not(.boardsection[data-board=day_' + num + '])').css({animation: "body 1s forwards"});
+  })
+
+
+
+
+
 //.boardsection[data-board]
 //const nextButton = document.querySelector( '#next' );
-
+/*
 const nextAction = function() {
   var num = 0;
   var day = document.getElementById( '.boardsection[data-board=day_' + num + ']' );
   var body = document.getElementById( ':not(.boardsection[data-board=day_' + num + '])' );
-  
-  day.style.color = "blue";
-  
+
+  //day.style.color = "blue";
+  body.css({animation: "nextbody 1s forwards"})
+*/
   //day.style.animation = "next 1s forwards";
   //body.css({animation: "nextbody 1s forwards"});
   
-}
+
           
 //animation: nextbody 1s forwards;
 //animation: next 1s forwards;
