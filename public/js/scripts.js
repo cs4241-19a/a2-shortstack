08d37ae54a253cc449f0af75a6ac3253b0227822
Glitch (paristhecity-a2-pllopez) <none>
2019-09-08 23:35:28 +0000

    🎇🏕 Checkpoint ./public/css/style.css:6266626/1334 ./public/js/scripts.js:6266626/367 ./public/index.html:6266626/156

--- public/js/scripts.js
+++ public/js/scripts.js
@@ -4,9 +4,7 @@ console.log("Welcome to assignment 2!")
 $(document).ready(function () {
   $(document).on('click', "#next", function() {
     var sections = $('.boardsection[data-board]');
-    $(".boardsection[data-board]:first-child").css({animation: "next 1s forwards"});
-
     sections.first().insertAfter(sections.last());
-    $(".boardsection[data-board]").css({animation: "next 1s forwards"});
+    sections.css({animation: "next 1s 1"});
   })
 })
