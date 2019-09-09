3ebbe1f8159dd738cccf07365f8b2e219960e401
Glitch (a2-benemrick) <none>
2019-09-09 00:30:15 +0000

    🍴🚃 Checkpoint ./public/js/scripts.js:2480412/2749 ./server.improved.js:2480412/6 ./public/index.html:2480412/774

--- public/js/scripts.js
+++ public/js/scripts.js
@@ -9,6 +9,34 @@ const unsplash = new Unsplash({
   secret: "fd62ab61acfdb6f6a1a725f41856dff34770e89a875c51224515c1c2aca625da"
 });
 
+const submit = function( e ) {
+  // prevent default form action from being carried out
+  e.preventDefault()
+
+  const input = document.querySelector( '#yourname' ),
+        json = { yourname: input.value },
+        body = JSON.stringify( json )
+
+  fetch( '/submit', {
+    method:'POST',
+    body 
+  })
+  .then( function( response ) {
+    // do something with the reponse 
+    console.log( response )
+  })
+
+  return false
+}
+        
+const url = function() {
+  unsplash.search.photos("shopping", 1)
+  .then(toJson)
+  .then(json => {
+    // Your code
+  });
+}
+
 const randPhoto = function() {
   unsplash.search.photos("dogs", 1)
   .then(toJson)
@@ -22,41 +50,27 @@ const loadFavorites = async function() {
   const data = await resp.json();
   const favs = data.data;
   
-   /*var html = "<table border='1|1'>";
-    for (var i = 0; i < rows.length; i++) {
-        html+="<tr>";
-        html+="<td>"+rows[i].name+"</td>";
-        html+="<td>"+rows[i].age+"</td>";
-        html+="<td>"+rows[i].email+"</td>";
-
-        html+="</tr>";
-
-    }
-    html+="</table>";
-    $("div").html(html);*/
+  let htmlCard = `<div class="card"> 
+                        <div class="card-body">`;
   
-  /*<div class="card">
-          <div class="card-body">
-            <h5 class="card-title">Yeezy</h5>
-            <p class="card-text"><small class="text-muted">Category</small></p>
-            <p class="card-text">Price</p>
-          </div>
-          <div class="card-footer bg-transparent">Rating</div>
-        </div>*/
-  
-  let htmlCard = `<div class="card">' 
-                        <div class="card-body">`
-  
-  
-  for (let i = 0; i < favs.length; i++) {
-    htmlCard = `<h5 class="card-title">${favs[i].name}</h5>
-
-    `
+  for (let i = 0; i < 3; i++) {
+    htmlCard += `<h5 class="card-title">${favs[i].name}</h5>
+                <p class="card-text"><small class="text-muted">${favs[i].category}</small></p>
+                <p class="card-text">${favs[i].usd}</p>
+                <p class="card-text">${favs[i].eur}</p>
+              </div>
+              <div class="card-footer bg-transparent">${favs[i].rating}</div>
+            </div>`;
   }
 
+  document.getElementById("favorites").innerHTML = htmlCard;
+
   return false;
-};
+}
 
 window.onload = function() {
-  loadFavorites()
-};
+  const button = document.querySelector( 'button' )
+  button.onclick = submit
+  
+  loadFavorites();
+}
--- server.improved.js
+++ server.improved.js
@@ -7,9 +7,9 @@ const http = require( 'http' ),
       port = 3000
 
 const appdata = [
-  { 'name': 'Yeezy 350', 'Categroy': 'Fashion', 'rating': 5, 'usd': 200, 'eur': 182 },
-  { 'name': 'Macbook Pro', 'Categroy': 'Tech', 'rating': 3, 'usd': 1299, 'eur': 1178.31 },
-  { 'name': 'Wilson Basketball', 'Categroy': 'Sports', 'rating': 2, 'usd': 20, 'eur': 18.14 },
+  { 'name': 'Yeezy 350', 'categroy': 'Fashion', 'rating': 5, 'usd': 200, 'eur': 182 },
+  { 'name': 'Macbook Pro', 'categroy': 'Tech', 'rating': 3, 'usd': 1299, 'eur': 1178.31 },
+  { 'name': 'Wilson Basketball', 'categroy': 'Sports', 'rating': 2, 'usd': 20, 'eur': 18.14 },
 ]
 
 const server = http.createServer( function( request,response ) {
