4d91d2c577499ad82632ff563886ed5f6ae96627
jcharante <git@jcharante.com>
2019-09-09 01:50:13 -0400

    remove unused functions

--- server.improved.js
+++ server.improved.js
@@ -48,20 +48,6 @@ const handleGet = function( request, response ) {
   }
 }
 
-/*
-Source for parseDate and datediff: https://stackoverflow.com/a/543152
- */
-function parseDate(str) {
-  var mdy = str.split('/');
-  return new Date(mdy[2], mdy[0]-1, mdy[1]);
-}
-
-function datediff(first, second) {
-  // Take the difference between the dates and divide by milliseconds per day.
-  // Round to nearest whole number to deal with DST.
-  return Math.round((second-first)/(1000*60*60*24));
-}
-
 const handlePost = function( request, response ) {
   let dataString = ''
 
