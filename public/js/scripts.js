09a2a24bbb40c0ef7d758fb66729fc5c2be7e5e0
kdreese <34196410+kdreese@users.noreply.github.com>
2019-09-09 11:44:56 -0400

    Update scripts.js

--- public/js/scripts.js
+++ public/js/scripts.js
@@ -1,3 +1,25 @@
-// Add some Javascript code here, to run on the front end.
-
 console.log("Welcome to assignment 2!")
+
+document.getElementById('topLeftBox').onclick = function flip(){
+  document.getElementById('topLeftBox').style = "background-color:red"        //change color to what is designated to it in the data set
+}
+
+document.getElementById('topMiddleBox').onclick = function flip(){
+  document.getElementById('topMiddleBox').style = "background-color:red"      //change color to what is designated to it in the data set
+}
+
+document.getElementById('topRightBox').onclick = function flip(){
+  document.getElementById('topRightBox').style = "background-color:red"       //change color to what is designated to it in the data set
+}
+
+document.getElementById('bottomLeftBox').onclick = function flip(){
+  document.getElementById('bottomLeftBox').style = "background-color:red"     //change color to what is designated to it in the data set
+}
+
+document.getElementById('bottomMiddleBox').onclick = function flip(){
+  document.getElementById('bottomMiddleBox').style = "background-color:red"   //change color to what is designated to it in the data set
+}
+
+document.getElementById('bottomRightBox').onclick = function flip(){
+  document.getElementById('bottomRightBox').style = "background-color:red"    //change color to what is designated to it in the data set
+}
