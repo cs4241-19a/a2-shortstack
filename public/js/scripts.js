b6c88b896b57a11c757f185d58773b7dff56ee6d
Glitch (a2-cbennet-cooper-bennet) <none>
2019-09-09 13:20:13 +0000

    🎖🎯 Updated with Glitch

--- public/js/scripts.js
+++ public/js/scripts.js
@@ -1,3 +1,37 @@
-// Add some Javascript code here, to run on the front end.
+function calc(){
+  let rest = document.getElementById("rest").value;
+  let date = document.getElementById("date").value;
+  let cost = document.getElementById("cost").value;
+  let server = document.getElementById("server").value;
+  let tip = (cost * .1* server).toFixed(2);
+  let total = (parseFloat(cost, 10) + parseFloat(tip,10)).toFixed(2);
+  let quality = "n/a";
+  if(server == 3){
+    quality = "Amazing";
+  }
+  else if(server == 2){
+    quality = "Average";
+  }
+  else if(server == 1){
+    quality = "Poor"
+  }
+  alert("Your tip should be: $" + tip);
+  row(rest, date, parseFloat(cost, 10).toFixed(2), quality, tip, total);
+}
 
-console.log("Welcome to assignment 2!")
+function row(rest, date, cost, server, tip, total) { 
+  var table =document.getElementById("past"); 
+  var NewRow = table.insertRow(-1); 
+  var Rest = NewRow.insertCell(0); 
+  var Date = NewRow.insertCell(1); 
+  var Cost = NewRow.insertCell(2); 
+  var Server = NewRow.insertCell(3); 
+  var Tip = NewRow.insertCell(4); 
+  var Total = NewRow.insertCell(5); 
+  Rest.innerHTML = rest; 
+  Date.innerHTML = date;
+  Cost.innerHTML = cost;
+  Server.innerHTML = server;
+  Tip.innerHTML = tip;
+  Total.innerHTML = total;
+} 
--- server.improved.js
+++ server.improved.js
@@ -6,12 +6,6 @@ const http = require( 'http' ),
       dir  = 'public/',
       port = 3000
 
-const appdata = [
-  { 'model': 'toyota', 'year': 1999, 'mpg': 23 },
-  { 'model': 'honda', 'year': 2004, 'mpg': 30 },
-  { 'model': 'ford', 'year': 1987, 'mpg': 14} 
-]
-
 const server = http.createServer( function( request,response ) {
   if( request.method === 'GET' ) {
     handleGet( request, response )    
@@ -38,9 +32,8 @@ const handlePost = function( request, response ) {
   })
 
   request.on( 'end', function() {
-    console.log( JSON.parse( dataString ) )
-
-    // ... do something with the data here!!!
+    
+    
 
     response.writeHead( 200, "OK", {'Content-Type': 'text/plain' })
     response.end()
