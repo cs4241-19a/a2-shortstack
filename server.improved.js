fec174f11b5b8c0528cc904634c3138302b95aa9
elizabethkirschner <eekirschner@wpi.edu>
2019-09-08 22:42:41 -0400

    Fully functional
    Signed-off-by: elizabethkirschner <eekirschner@wpi.edu>

--- server.improved.js
+++ server.improved.js
@@ -7,15 +7,15 @@ const http = require( 'http' ),
       port = 3000
 
 const appdata = [
-  { 'model': 'toyota', 'year': 1999, 'mpg': 23 },
-  { 'model': 'honda', 'year': 2004, 'mpg': 30 },
-  { 'model': 'ford', 'year': 1987, 'mpg': 14} 
+  { 'title': 'Brave New World', 'author': 'Aldous Huxley', 'genre': 'Fiction', 'numPages':250,'pagesRead':100, 'percentRead':40 },
+  { 'title': 'Catcher in the Rye', 'author': 'J.D. Salinger', 'genre': 'Fiction', 'numPages':250,'pagesRead':100, 'percentRead':40 },
 ]
 
 const server = http.createServer( function( request,response ) {
-  if( request.method === 'GET' ) {
+  if ( request.method === 'GET' ) {
     handleGet( request, response )    
-  }else if( request.method === 'POST' ){
+  }
+  else if ( request.method === 'POST' ) {
     handlePost( request, response ) 
   }
 })
@@ -23,28 +23,110 @@ const server = http.createServer( function( request,response ) {
 const handleGet = function( request, response ) {
   const filename = dir + request.url.slice( 1 ) 
 
-  if( request.url === '/' ) {
+  if ( request.url === '/' ) {
     sendFile( response, 'public/index.html' )
-  }else{
+  }
+  else if ( request.url == '/getdata' ){
+    getdata( response )
+  }
+  else {
     sendFile( response, filename )
   }
 }
 
 const handlePost = function( request, response ) {
+  if ( request.url === '/newEntry') {
+    newEntry( request, response )
+  }
+  else if ( request.url === '/editEntry' ) {
+    editEntry( request, response )
+  }
+  else if ( request.url === '/deleteEntry' ) {
+    deleteEntry( request, response )
+  }
+  else {
+    response.writeHeader( 404 )
+    response.end( '404 Error: File Not Found' )
+  }
+}
+
+const deleteEntry = function( request, response ) {
   let dataString = ''
 
   request.on( 'data', function( data ) {
-      dataString += data 
+    dataString += data 
   })
 
   request.on( 'end', function() {
-    console.log( JSON.parse( dataString ) )
+    var entry = JSON.parse(dataString)
+    for ( var i = 0; i < appdata.length; i++) {
+      if (appdata[i].title === entry.title) {
+        appdata.splice( i, 1)
+      }
+    }
 
-    // ... do something with the data here!!!
-
-    response.writeHead( 200, "OK", {'Content-Type': 'text/plain' })
-    response.end()
+    response.writeHead( 200, "OK", {'Content-Type': 'application/json'} )
+    var json = JSON.stringify( appdata )
+    response.end( json )
   })
+}
+
+const editEntry = function( request, response ) {
+  let dataString = ''
+
+  request.on( 'data', function( data ) {
+    dataString += data 
+  })
+
+  request.on( 'end', function() {
+    console.log( "data string " + JSON.parse(dataString) )
+    const entry = JSON.parse(dataString).entryKey,
+          newEntry = JSON.parse(dataString).newEntry,
+          percent = calculatePercentRead( newEntry )
+          newEntry.percentRead = percent
+
+    for ( var i = 0; i < appdata.length; i++) {
+      if (appdata[i].title === entry) {
+        appdata.splice( i, 1, newEntry )
+      }
+    }
+
+    response.writeHead( 200, "OK", {'Content-Type': 'application/json'} )
+    var json = JSON.stringify( appdata )
+    response.end( json )
+  })
+}
+
+const newEntry = function( request, response ) {
+  let dataString = ''
+
+  request.on( 'data', function( data ) {
+    dataString += data 
+  })
+
+  request.on( 'end', function() {
+    console.log( JSON.parse( dataString ))
+    
+    const dataJson = JSON.parse( dataString ),
+          percent = calculatePercentRead( dataJson )
+    dataJson.percentRead = percent
+    appdata.push( dataJson )
+
+    response.writeHead( 200, "OK", {'Content-Type': 'application/json' })
+    var json = JSON.stringify( appdata )
+    response.end(json);
+  })
+}
+
+const calculatePercentRead = function( dataJson ) {
+  return (dataJson.pagesRead / dataJson.numPages ) * 100
+}
+
+const getdata = function( response ) {
+  response.writeHead( 200, "OK", {"Content-Type":"application/json"})
+
+  var json = JSON.stringify( appdata )
+  response.end(json)
 }
 
 const sendFile = function( response, filename ) {
@@ -54,17 +136,14 @@ const sendFile = function( response, filename ) {
 
      // if the error = null, then we've loaded the file successfully
      if( err === null ) {
-
        // status code: https://httpstatuses.com
        response.writeHeader( 200, { 'Content-Type': type })
        response.end( content )
-
-     }else{
-
+     }
+     else {
        // file not found, error code 404
        response.writeHeader( 404 )
        response.end( '404 Error: File Not Found' )
-
      }
    })
 }
