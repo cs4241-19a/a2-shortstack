fdbac8c89603e67901a41ee50f58199f5bc612a2
kaitlinmcdermott <37385385+kaitlinmcdermott@users.noreply.github.com>
2019-09-09 11:54:00 -0400

    Update scripts.js

--- public/js/scripts.js
+++ public/js/scripts.js
@@ -1,3 +1,70 @@
-// Add some Javascript code here, to run on the front end.
+const form = document.querySelector('form')
+const ul = document.querySelector('ul')
+const button = document.querySelector('button')
+const input = document.getElementById('name')
+let itemsArray = localStorage.getItem('names') ? JSON.parse(localStorage.getItem('names')) : []
 
-console.log("Welcome to assignment 2!")
+localStorage.setItem('names', JSON.stringify(itemsArray))
+const data = JSON.parse(localStorage.getItem('names'))
+
+const liMaker = text => {
+    const li = document.createElement('li')
+    li.textContent = text
+    ul.appendChild(li)
+}
+
+form.addEventListener('submit', function(e) {
+    e.preventDefault()
+
+    itemsArray.push(input.value)
+    localStorage.setItem('names', JSON.stringify(itemsArray))
+    liMaker(input.value)
+    input.value = ''
+})
+
+data.forEach(item => {
+    liMaker(item)
+})
+
+button.addEventListener('click', function() {
+    localStorage.clear()
+    while (ul.firstChild) {
+        ul.removeChild(ul.firstChild)
+    }
+})
+
+function getBMI () {
+    //Obtain user inputs
+    var height=Number(document.getElementById("height").value);
+    var heightunits=document.getElementById("heightunits").value;
+    var weight=Number(document.getElementById("weight").value);
+    var weightunits=document.getElementById("weightunits").value;
+
+
+    //Convert all units to metric
+    if (heightunits=="inches") height/=39.3700787;
+    if (weightunits=="lb") weight/=2.20462;
+
+    //Perform calculation
+    var BMI=weight/Math.pow(height,2);
+
+    //Display result of calculation
+    document.getElementById("output").innerText=Math.round(BMI*100)/100;
+
+}
+var submissions=[];
+function saveSubmissions(){
+    var submission = document.getElementById("name").value;
+    submissions.push("name");
+    console.log(submissions);
+    document.getElementById("finalBMI").value=submissions;
+    console.log(document.getElementById("finalBMI").value);
+}
+document.getElementById("name").innerHTML = submissions;
+
+function write_below(form)
+{
+    var output = document.forms.write.output_towrite.value;
+    document.getElementById('write_here').innerHTML="Your input was:"+output;
+    return false;
+}
