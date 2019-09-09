f86c846bb0ca107bd5d537e08ba0410e0b42be79
Joseph Yuen <josephnewbox@gmail.com>
2019-09-08 20:26:32 -0400

    laid basic words and column configuration to make forms side by side. also transferred script in index.html to scripts.js.

--- server.improved.js
+++ server.improved.js
@@ -25,7 +25,7 @@ const handleGet = function( request, response ) {
 
   if( request.url === '/' ) {
     sendFile( response, 'public/index.html' )
-  }else{
+  } else{
     sendFile( response, filename )
   }
 }
