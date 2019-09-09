5292e133e5513b2eaff88a1dce42da97f0936817
Elie <eliemhess@gmail.com>
2019-09-08 21:18:49 -0400

    Finished
    Complete project (except README)

--- server.improved.js
+++ server.improved.js
@@ -1,95 +1,136 @@
-const http = require( "http" ),
-      fs   = require( "fs" ),
-      // IMPORTANT: you must run `npm install` in the directory for this assignment
-      // to install the mime library used in the following line of code
-      mime = require( "mime" ),
-      dir  = "public/",
-      port = 3000
+const http = require("http"),
+  fs = require("fs"),
+  mime = require("mime"),
+  dir = "public/",
+  port = 3000;
 
-const  appdata = [
-  { "firstname": "Sheev", "lastname":  "Palpatine", "major": "Emperor", "number": 2 },
-  { "firstname": "Obi-Wan", "lastname":  "Kenobi", "major": "Jedi (alumn)", "number": 1 },
-  { "firstname": "Luke", "lastname":  "Skywalker", "major": "Jedi", "number": 3 }
-]
-
-const server = http.createServer( function( request,response ) {
-  if( request.method === "GET" ) {
-    handleGet( request, response )
-  }else if( request.method === "POST" ){
-    handlePost( request, response )
+const appdata = [{
+    "firstname": "Luke",
+    "lastname": "Skywalker",
+    "major": "Lightsaber Construction",
+    "year": 1596,
+    "graduated": "Yes",
+    "velocity": "42 m/s"
+  },
+  {
+    "firstname": "Obi-Wan",
+    "lastname": "Kenobi",
+    "major": "Taking the High Ground",
+    "year": 1554,
+    "graduated": "Yes",
+    "velocity": "11 m/s"
   }
-})
+];
 
-const handleGet = function( request, response ) {
-  const filename = dir + request.url.slice( 1 )
+const server = http.createServer(function(request, response) {
+  if (request.method === "GET") {
+    handleGet(request, response);
+  } else if (request.method === "POST") {
+    handlePost(request, response);
+  }
+});
 
-  if( request.url === "/" ) {
-    console.log("dsakfjhasldiufdsakfhj")
-    sendFile( response, "public/index.html" )
+const handleGet = function(request, response) {
+  const filename = dir + request.url.slice(1);
+  if (request.url === "/") {
+    sendFile(response, "public/index.html")
   } else if (request.url === "/getdata") {
-    sendData(response, request)
-  }else{
-    sendFile( response, filename )
+    sendData(response, appdata);
+  } else {
+    sendFile(response, filename);
   }
-}
+};
+
+const handlePost = function(request, response) {
+  let dataString = "";
+
+  request.on("data", function(data) {
+    dataString += data
+  });
+
+  request.on("end", function() {
+    switch (request.url) {
+      case "/submit":
+        const data = JSON.parse(dataString);
+
+        const graduated = parseInt(data.year) < 2019 ? "Yes" : "No";
+
+        const newMember = {
+          "firstname": data.firstname,
+          "lastname": data.lastname,
+          "major": data.major,
+          "year": data.year,
+          "graduated": graduated,
+          "velocity": data.velocity
+        };
+        appdata.push(newMember);
+        response.writeHead(200, "OK", {
+          "Content-Type": "text/plain"
+        });
+        response.end();
+
+        break;
+      case "/update":
+        const updatedData = JSON.parse(dataString);
+
+        const updatedGraduated = parseInt(updatedData.year) < 2019 ? "Yes" : "No";
+
+        const updatedEntry = {
+          "firstname": updatedData.firstname,
+          "lastname": updatedData.lastname,
+          "major": updatedData.major,
+          "year": updatedData.year,
+          "graduated": updatedGraduated,
+          "velocity": updatedData.velocity
+        };
+        appdata.splice(updatedData.index, 1, updatedEntry);
+        response.writeHead(200, "OK", {
+          "Content-Type": "text/plain"
+        });
+        response.end();
+        break;
+
+      case "/delete":
+        const entryToDelete = JSON.parse(dataString);
+        appdata.splice(entryToDelete.memberNum, 1);
+        response.writeHead(200, "OK", {
+          "Content-Type": "text/plain"
+        });
+        response.end();
+        break;
+
+      default:
+        response.end("404 Error: File Not Found");
+        break;
+    }
+  })
+};
 
 const sendData = function(response, request) {
-  request.on("end", function() {
-    response.writeHead( 200, "OK", {"Content-Type": "text/plain" })
-    response.write(JSON.stringify(appdata))
-    response.end()
-  })
-}
+  const type = mime.getType(request);
+  response.writeHeader(200, {
+    "Content-Type": type
+  });
+  response.write(JSON.stringify({
+    data: request
+  }));
+  response.end();
+};
 
-const handlePost = function( request, response ) {
-  let dataString = ""
+const sendFile = function(response, filename) {
+  const type = mime.getType(filename);
 
-  request.on( "data", function( data ) {
-      dataString += data
-      console.log("dataString: ", dataString);
-  })
-
-  request.on( "end", function() {
-    const data = JSON.parse(dataString)
-
-    if(request.url === "/add") {
-      let foundName = false
-      for(let i = 0; i < appdata.length; i++){
-        if(appdata[i].name === data.name) foundName = true;
-      }
-      if(foundName === false) {
-        appdata.push(data)
-      }
+  fs.readFile(filename, function(err, content) {
+    if (err === null) {
+      response.writeHeader(200, {
+        "Content-Type": type
+      });
+      response.end(content)
+    } else {
+      response.writeHeader(404);
+      response.end("404 Error: File Not Found");
     }
-    console.log( data )
-    if(request.url)
-    // ... do something with the data here!!!
-
-    response.writeHead( 200, "OK", {"Content-Type": "text/plain" })
-    response.end()
   })
-}
+};
 
-const sendFile = function( response, filename ) {
-   const type = mime.getType( filename )
-
-   fs.readFile( filename, function( err, content ) {
-
-     // if the error = null, then we"ve loaded the file successfully
-     if( err === null ) {
-
-       // status code: https://httpstatuses.com
-       response.writeHeader( 200, { "Content-Type": type })
-       response.end( content )
-
-     }else{
-
-       // file not found, error code 404
-       response.writeHeader( 404 )
-       response.end( "404 Error: File Not Found" )
-
-     }
-   })
-}
-
-server.listen( process.env.PORT || port )
+server.listen(process.env.PORT || port);
