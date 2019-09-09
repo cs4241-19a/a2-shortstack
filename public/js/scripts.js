5810016234223fa5119e57bb4995ef56deceb484
jefff <ryan@Ryan.local>
2019-09-08 21:19:28 -0400

    website is done

--- public/js/scripts.js
+++ public/js/scripts.js
@@ -1,3 +1,72 @@
-// Add some Javascript code here, to run on the front end.
+ var startingClass =[
+    { "Department" : "CS1101",
+      "Professor"  : "bob",
+      "Name" : "programming design concepts"
+    },
+    { "Department"  : "ME1800",
+      "Professor"  : "Jill",
+      "Name" : "prototyping"
+    },
+    { "Department"  : "CS2303",
+      "Professor"  : "Andres",
+      "Name" : "systems"
+    },
+    { "Department"  : "PSY1401",
+      "Professor"  : "Brian",
+      "Name" : "cognitive psych"
+    },
+    { "Department"  : "RBE1001",
+      "Professor"  : "Megan",
+      "Name" : "intro to robotics"
+    },
+    { "Department"  : "ECE2049",
+      "Professor"  : "Sam",
+      "Name" : "embedded design"
+    },
+    { "Department"  : "CS4801",
+      "Professor"  : "Emily",
+      "Name" : "crypto"
+    }
+  ]
+  var list = [".", "Dept code of Class", "Professor Teaching Class", "Name of Class"];
+  var bodyTag = document.getElementsByClassName("body");
+  bodyTag.addEventListener("load", myFunction());
+  var deptArr = ["Test"];
+  var profArr = [];
+  var nameArr = [];
+  var classesByCat = {
+    Add: deptArr,
+    Modify: profArr,
+    Delete: nameArr
+  }
+  var testing;
 
-console.log("Welcome to assignment 2!")
+  function myFunction() {   
+    var table = document.getElementById("results");
+    var header = table.createTHead();
+    var row = header.insertRow(0);
+    for(var i in list){
+      var cell = row.insertCell(0);
+      cell.innerHTML = "<strong>"+list[3-i]+"</strong>";
+    }
+    for(var i in startingClass){
+      row = header.insertRow()
+      cell = row.insertCell()
+      cell.innerHTML = i;
+      for(var j = 1; j < 4; j++){
+        cell = row.insertCell(j);
+        if(j == 1){
+          cell.innerHTML = startingClass[i].Department;
+        }
+        else if(j == 2){
+          cell.innerHTML = startingClass[i].Professor;
+        }
+        else{
+          cell.innerHTML = startingClass[i].Name;
+        }
+      }
+    } 
+  }
+    
+   
+
--- server.improved.js
+++ server.improved.js
@@ -3,14 +3,42 @@ const http = require( 'http' ),
       // IMPORTANT: you must run `npm install` in the directory for this assignment
       // to install the mime library used in the following line of code
       mime = require( 'mime' ),
+      TAFFY = require('taffy'),
       dir  = 'public/',
-      port = 3000
+      port = 3000    
 
-const appdata = [
-  { 'model': 'toyota', 'year': 1999, 'mpg': 23 },
-  { 'model': 'honda', 'year': 2004, 'mpg': 30 },
-  { 'model': 'ford', 'year': 1987, 'mpg': 14} 
-]
+
+var classes = TAFFY([
+    { "Department" : "CS1101",
+      "Professor" : "bob",
+      "Name" : "programming design concepts"
+    },
+    { "Department" : "ME1800",
+      "Professor" : "Jill",
+      "Name" : "prototyping"
+    },
+    { "Department"  : "CS2303",
+      "Professor" : "Andres",
+      "Name" : "systems"
+    },
+    { "Department" : "PSY1401",
+      "Professor" : "Brian",
+      "Name" : "cognitive psych"
+    },
+    { "Department" : "RBE1001",
+      "Professor" : "Megan",
+      "Name" : "intro to robotics"
+    },
+    { "Department" : "ECE2049",
+      "Professor" : "Sam",
+      "Name" : "embedded design"
+    },
+    { "Department" : "CS4801",
+      "Professor" : "Emily",
+      "Name" : "crypto"
+    }
+]);
+
 
 const server = http.createServer( function( request,response ) {
   if( request.method === 'GET' ) {
@@ -20,30 +48,82 @@ const server = http.createServer( function( request,response ) {
   }
 })
 
+
+
 const handleGet = function( request, response ) {
   const filename = dir + request.url.slice( 1 ) 
 
   if( request.url === '/' ) {
     sendFile( response, 'public/index.html' )
-  }else{
+  }
+  else if(request.url === '/scripts.js'){
+    sendFile(response, "public/js/scripts.js")
+  }
+  else if(request.url === '/style.css'){
+    sendFile(response, "public/css/style.css")
+  }
+  else if(request.url === '/ac8c900b-49be-4128-8edb-e64905679f74%2FWPI_logo.png?v=1567371575005'){
+    sendFile(response, "ac8c900b-49be-4128-8edb-e64905679f74%2FWPI_logo.png?v=1567371575005")
+  }
+  else{
     sendFile( response, filename )
   }
 }
 
 const handlePost = function( request, response ) {
   let dataString = ''
-
+  let flag = 0;
   request.on( 'data', function( data ) {
       dataString += data 
   })
-
   request.on( 'end', function() {
-    console.log( JSON.parse( dataString ) )
-
-    // ... do something with the data here!!!
-
-    response.writeHead( 200, "OK", {'Content-Type': 'text/plain' })
-    response.end()
+    let inputData = JSON.parse( dataString )
+    let action = inputData.action
+    if(action.includes('a')){
+      let dept = inputData.dept
+      let prof = inputData.prof
+      let name = inputData.name
+      let myEnt = {}
+      myEnt["Department"] = dept;
+      myEnt["Professor"] = prof
+      myEnt["Name"] = name
+      console.log(myEnt)
+      classes.insert(myEnt);
+    }
+    else if(action.includes('m')){
+      let cat = inputData.category;
+      let catTwo = inputData.secCategory;
+      let inputVal = inputData.input;
+      let catObj = {};
+      let innerObj = {};
+      innerObj["like"] = catTwo
+      catObj[cat] = innerObj;
+      let updateObj = {}
+      updateObj[cat] = inputVal;
+      console.log(updateObj)
+      classes(catObj).update(updateObj);
+    }
+    else{
+      let cat = inputData.category;
+      let catTwo = inputData.secCategory;
+      let inputVal = inputData.input;
+      let catObj = {};
+      let innerObj = {}
+      innerObj["like"] = catTwo
+      catObj[cat] = innerObj;
+      let updateObj = {}
+      updateObj[cat] = null;
+      console.log(updateObj)
+      classes(catObj).remove();
+    }
+    if(!flag){
+      let json, finalJson = [];
+      let sendjson;
+      response.writeHead( 200, "OK", {'Content-Type': 'application/json', 'charset':'UTF-8'});
+      classes().each(function (r){json = {dept:r.Department ,prof:r.Professor ,name:r.Name}; finalJson.push(json);})
+      sendjson={objs:finalJson}
+      response.end(JSON.stringify(sendjson));
+    }
   })
 }
 
@@ -55,7 +135,7 @@ const sendFile = function( response, filename ) {
      // if the error = null, then we've loaded the file successfully
      if( err === null ) {
 
-       // status code: https://httpstatuses.com
+       //status code: https://httpstatuses.com
        response.writeHeader( 200, { 'Content-Type': type })
        response.end( content )
 
