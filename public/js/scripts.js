ef58d84ef4c5dfb84610dd46f91c355d3afb91e6
Joseph Yuen <josephnewbox@gmail.com>
2019-09-09 01:10:57 -0400

    added functions for calculating cost and order number and then added results to json

--- public/js/scripts.js
+++ public/js/scripts.js
@@ -1,20 +1,34 @@
-var name = document.getElementById('name')
+
+// order form fields
+var yourname = document.getElementById('name')
 var phone = document.getElementById('phone')
 var potato = document.getElementById('potato')
 var seasoning = document.getElementById('seasoning')
 var size = document.getElementById('size')
 
+// keeps track of latest order number
+var orderNum = 0;
+
 // add order to server queue table
 const submit = function( e ) {
 
-     // prevent default form action from being carried out
+    // prevent default form action from being carried out
     e.preventDefault()
-    const name = document.querySelector( '#name' ),
+    const yourname = document.querySelector( '#name' ),
           phone = document.querySelector( '#phone' ),
           potato = document.querySelector( '#potato' ),
           seasoning = document.querySelector( '#seasoning' ),
-          size = document.querySelector( '#size' ),
-          json = { name: name.value, phone: phone.value, potato: potato.value, seasoning: seasoning.value, size: size.value},
+          size = document.querySelector( '#size' )
+
+    // calculate cost of order
+    var cost = calculateCost(potato.value, seasoning.value, size.value);
+
+    // increment order number for a new order
+    orderNum++;
+
+    console.log("Order #" + orderNum + " Cost = $" + cost);
+
+    const json = { yourname: yourname.value, phone: phone.value, potato: potato.value, seasoning: seasoning.value, size: size.value, cost: cost, orderNum: orderNum},
           body = JSON.stringify( json )
 
     fetch( '/submit', {
@@ -27,6 +41,8 @@ const submit = function( e ) {
       console.log( response )
     })
 
+    resetForm();
+
     return false
 }
 
@@ -36,4 +52,82 @@ window.onload = function() {
     button.onclick = submit
 }
 
-console.log("Welcome to assignment 2!")
+// resets form fields
+function resetForm() {
+  yourname.value = 'John Doe';
+  phone.value = "123-456-7890";
+  potato.value = "yellow";
+  seasoning.value = "none";
+  size.value = "small";
+  console.log("Form fields reset");
+}
+
+// calculates order Cost
+function calculateCost(potato, seasoning, size) {
+  var cost = 0;
+
+  // potato keys
+  switch(potato) {
+    case "yellow":
+      cost += 3;
+      break;
+    case "red":
+      cost += 4;
+      break;
+    case "idaho":
+      cost += 5;
+      break;
+    case "sweet":
+      cost += 6;
+      break;
+    default:
+      console.log("Potato Error");
+  }
+
+  // seasoning keys
+  switch(seasoning) {
+    case "none":
+      cost += 0;
+      break;
+    case "salt":
+      cost += 1;
+      break;
+    case "pepper":
+      cost += 1;
+      break;
+    case "snp":
+      cost += 2;
+      break;
+    case "cajun":
+      cost += 3;
+      break;
+    case "guys":
+      cost += 3;
+      break;
+    case "original":
+      cost += 3;
+      break;
+    default:
+      console.log("Seasoning Error");
+  }
+
+  // size keys
+  switch(size) {
+    case "small":
+      cost = cost*1;
+      break;
+    case "medium":
+      cost = cost*1.5;
+      break;
+    case "large":
+      cost = cost*2;
+      break;
+    default:
+      console.log("Size Error");
+  }
+
+  return cost;
+}
+
+console.log("a2-jhyuen")
+console.log("Welcome to Fantastic Fries!")
