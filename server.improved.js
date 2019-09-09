764a2fceeb4b12d4bc616e9ddb4ca42e5d619bac
Peter Christakos <newtownfam@gmail.com>
2019-09-08 21:48:36 -0400

    removed console.log that caused error

--- server.improved.js
+++ server.improved.js
@@ -45,8 +45,6 @@ const handlePost = function( request, response ) {
     // append to data array 
     tableData.push(JSON.parse(dataString))
 
-    console.log("Server Dataset: " + (JSON.parse(tableData)))
-
     response.writeHead( 200, "OK", {'Content-Type': 'text/plain' })
     response.end()
   })
