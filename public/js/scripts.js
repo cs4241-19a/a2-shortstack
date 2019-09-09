f8ebc7c14b8510095e0fc180bcd064f8f3c0f270
Glitch (a2-benemrick) <none>
2019-09-09 00:42:14 +0000

    🌨🌃 Checkpoint ./public/js/scripts.js:2480412/1208 ./public/index.html:2480412/3469

--- public/js/scripts.js
+++ public/js/scripts.js
@@ -1,14 +1,3 @@
-// ES Modules syntax
-import Unsplash from 'unsplash-js';
-
-// require syntax
-const Unsplash = require('unsplash-js').default;
-
-const unsplash = new Unsplash({
-  applicationId: "210c371817cd6adae5a2fd6484fb09a9c92f0fec9d3443744a797518224071ef",
-  secret: "fd62ab61acfdb6f6a1a725f41856dff34770e89a875c51224515c1c2aca625da"
-});
-
 const submit = function( e ) {
   // prevent default form action from being carried out
   e.preventDefault()
@@ -45,32 +34,3 @@ const randPhoto = function() {
   });
 }
 
-const loadFavorites = async function() {
-  const resp = await fetch('/', { method: 'GET' });
-  const data = await resp.json();
-  const favs = data.data;
-  
-  let htmlCard = `<div class="card"> 
-                        <div class="card-body">`;
-  
-  for (let i = 0; i < 3; i++) {
-    htmlCard += `<h5 class="card-title">${favs[i].name}</h5>
-                <p class="card-text"><small class="text-muted">${favs[i].category}</small></p>
-                <p class="card-text">${favs[i].usd}</p>
-                <p class="card-text">${favs[i].eur}</p>
-              </div>
-              <div class="card-footer bg-transparent">${favs[i].rating}</div>
-            </div>`;
-  }
-
-  document.getElementById("favorites").innerHTML = htmlCard;
-
-  return false;
-}
-
-window.onload = function() {
-  const button = document.querySelector( 'button' )
-  button.onclick = submit
-  
-  loadFavorites();
-}
