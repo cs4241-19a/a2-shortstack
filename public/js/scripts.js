64778ae02ed49a11edf2a2b28d316d351f0ab007
Alexander-Rus <alex.rus10@gmail.com>
2019-09-09 09:28:01 -0400

    Bulk of code

--- public/js/scripts.js
+++ public/js/scripts.js
@@ -1,3 +1,83 @@
-// Add some Javascript code here, to run on the front end.
-
-console.log("Welcome to assignment 2!")
+window.onload = function(){
+    displayTable(); 
+  }
+  
+  var runningCount = 3.5
+  var myflag = 0
+  
+  //Delete a song from the list
+  function DeleteSong(id){
+    myflag = 1;
+    const json = { 
+                  'user': null,
+                  'id': id }
+    const body = JSON.stringify( json )
+    fetch( '/deleteSong', {
+    method:'POST',
+    body
+    })
+    .then( function(response) {
+    displayTable();
+    })
+  }
+  
+  function displayTable(){ 
+    const json = {user: null},
+          body = JSON.stringify(json)
+          fetch('/displayTable', {
+          method:'POST',
+          body
+        })
+      .then(function(response) {
+          response.json().then(data => {
+            document.getElementById("Song_Table").innerHTML = ""
+            addRow("Song Name", "Artist", "Song Length", "Picture URL", "Link to Video", "Delete Song")
+            const len = data.items.length
+            if (myflag === 1) {
+              runningCount = runningCount - (data.items[len - 1])
+            } else {
+              if (data.items[len - 1] ===NaN) {
+                runningCount = runningCount + 0
+              } else {
+                let iKim = data.items[len - 1]
+                runningCount =  parseFloat(runningCount) + parseFloat(iKim.newLength)
+                console.log(runningCount)
+              }
+            }
+            document.getElementById('count').innerHTML = runningCount
+            for(let k = 0; k < len; k++){
+              let iTim = data.items[k];
+              addRow(iTim.songName, iTim.artist, iTim.newLength, iTim.picture, iTim.video, "<button onclick='DeleteSong("+ iTim.id +")'>Delete Row</button>")
+  
+            }
+          })   
+      })    
+  }
+  
+  //Add the row to the table
+  function addRow(songName, artist, newLength, picture, video, modifyItem, deleteItem){ 
+    document.getElementById("Song_Table").innerHTML += "<tr><th>" + songName + "</th><th>" + artist + "</th><th>" + newLength + "</th><th>" + picture + "</th><th>" + video + "</th><th>" + modifyItem + "</th><th>"+ deleteItem + "</th></tr>";
+  }
+  const add_Song_Row = document.getElementById("add_song");
+  add_Song_Row.addEventListener("click", function(event) {
+    event.preventDefault();
+    const input = document.getElementById('new'),
+          json = { user: null,
+                   task: {
+                     'songName': input.elements["songName"].value,
+                     'artist': input.elements["artist"].value,
+                     'newLength': input.elements['newLength'].value,
+                     'picture': input.elements["picture"].value,
+                     'video': input.elements['video'].value
+                   }
+                },
+          body = JSON.stringify( json )
+    console.log(body)
+    fetch( '/addSong', {
+        method:'POST',
+        body
+        })
+    .then( function( response ) {
+      displayTable();
+    })
+  } );
--- server.improved.js
+++ server.improved.js
@@ -6,10 +6,28 @@ const http = require( 'http' ),
       dir  = 'public/',
       port = 3000
 
-const appdata = [
-  { 'model': 'toyota', 'year': 1999, 'mpg': 23 },
-  { 'model': 'honda', 'year': 2004, 'mpg': 30 },
-  { 'model': 'ford', 'year': 1987, 'mpg': 14} 
+const song_list = [
+  {name:'root',
+   items:[
+     {
+       'songName':'bye bye bye',
+       'artist': 'nsync',
+       'newLength': '3.5',
+       'picture': 'https://www.rollingstone.com/wp-content/uploads/2019/04/nsync-lead.jpg?resize=900,600&w=440',
+       'video':'https://www.youtube.com/watch?v=Eo-KmOd3i7s',
+       'id':'0',
+     },
+     {
+      'songName':'Another one bites the Dust',
+      'artist': 'Queen',
+      'newLength': '4',
+      'picture': 'https://www.rollingstone.com/wp-content/uploads/2019/04/nsync-lead.jpg?resize=900,600&w=440',
+      'video':'https://www.youtube.com/watch?v=Eo-KmOd3i7s',
+      'id':'1',
+    },
+   ]
+   
+   },
 ]
 
 const server = http.createServer( function( request,response ) {
@@ -17,17 +35,26 @@ const server = http.createServer( function( request,response ) {
     handleGet( request, response )    
   }else if( request.method === 'POST' ){
     handlePost( request, response ) 
-  }
+  } 
 })
 
 const handleGet = function( request, response ) {
-  const filename = dir + request.url.slice( 1 ) 
-
-  if( request.url === '/' ) {
-    sendFile( response, 'public/index.html' )
-  }else{
-    sendFile( response, filename )
-  }
+  switch( request.url ) {
+    case '/':
+      sendFile( response, 'public/index.html' )
+      break
+    case '/index.html':
+      sendFile( response, 'public/index.html' )
+      break
+    case '/style.css':
+      sendFile( response, 'public/css/style.css')
+      break
+    case '/scripts.js':
+      sendFile( response, 'public/js/scripts.js')
+      break
+    default:
+      response.end( '404 Error: File Not Found' )
+  } 
 }
 
 const handlePost = function( request, response ) {
@@ -36,12 +63,34 @@ const handlePost = function( request, response ) {
   request.on( 'data', function( data ) {
       dataString += data 
   })
-
+    
   request.on( 'end', function() {
     console.log( JSON.parse( dataString ) )
 
-    // ... do something with the data here!!!
+    //This is where you add the code
+        
+    switch(request.url) { 
+      case '/displayTable': 
+        const data = song_list[0]
+        addTo_Table(response, data)
+        break
 
+      case '/addSong': 
+        const taskData = JSON.parse(dataString)
+        const song = taskData.task
+        const taskId = addId(song)
+        song_list[0].items.push(song)
+        break
+
+      case '/deleteSong': 
+        const delete_Data = JSON.parse(dataString)
+        const ID = delete_Data.id 
+        const deleteThis = deleteSong(ID)
+        break 
+      default: 
+        response.end( '404 Error: File Not Found' )
+    }
+    
     response.writeHead( 200, "OK", {'Content-Type': 'text/plain' })
     response.end()
   })
@@ -60,13 +109,35 @@ const sendFile = function( response, filename ) {
        response.end( content )
 
      }else{
-
+       
        // file not found, error code 404
        response.writeHeader( 404 )
        response.end( '404 Error: File Not Found' )
-
      }
    })
 }
+
+// Delete the song from the master list
+function deleteSong(id){ 
+  song_list[0].items = song_list[0].items.filter(function(x){ 
+     return x.id != id 
+  })
+}
+
+const addTo_Table = function(response, json) { 
+  const type = mime.getType(json) 
+  console.log(JSON.stringify(json))
+  response.writeHeader(200, {'Content-Type': type })
+  response.end(JSON.stringify(json))
+}
+
+// Each song will have an ID associated with it to keep track of
+let ID = 2
+function addId(song){ 
+  song.id = ID
+  ID = ID + 1
+}
+
+//This is where it ends
 
 server.listen( process.env.PORT || port )
