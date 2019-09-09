6cc75dcaf0625718874570014c198d1995bbad7f
jyxiao1 <jyxiao@wpi.edu>
2019-09-09 11:56:42 -0400

    initial commit

--- server.improved.js
+++ server.improved.js
@@ -4,13 +4,11 @@ const http = require( 'http' ),
       // to install the mime library used in the following line of code
       mime = require( 'mime' ),
       dir  = 'public/',
-      port = 3000
+      port = 3000;
 
 const appdata = [
-  { 'model': 'toyota', 'year': 1999, 'mpg': 23 },
-  { 'model': 'honda', 'year': 2004, 'mpg': 30 },
-  { 'model': 'ford', 'year': 1987, 'mpg': 14} 
-]
+  { 'username': 'suedoenym', 'name': "John Doe", 'shipping address': "123 Fakeaddress st, Worcester, MA", 'cart': [] }
+];
 
 const server = http.createServer( function( request,response ) {
   if( request.method === 'GET' ) {
@@ -18,37 +16,45 @@ const server = http.createServer( function( request,response ) {
   }else if( request.method === 'POST' ){
     handlePost( request, response ) 
   }
-})
+});
 
 const handleGet = function( request, response ) {
-  const filename = dir + request.url.slice( 1 ) 
+  const filename = dir + request.url.slice( 1 );
 
   if( request.url === '/' ) {
     sendFile( response, 'public/index.html' )
   }else{
     sendFile( response, filename )
   }
-}
-
+};
 const handlePost = function( request, response ) {
-  let dataString = ''
+  let dataString = '';
 
   request.on( 'data', function( data ) {
       dataString += data 
-  })
+  });
 
   request.on( 'end', function() {
-    console.log( JSON.parse( dataString ) )
-
-    // ... do something with the data here!!!
-
-    response.writeHead( 200, "OK", {'Content-Type': 'text/plain' })
+    console.log( JSON.parse( dataString ) );
+    response.writeHead( 200, "OK", {'Content-Type': 'text/plain' });
+    response.write(JSON.stringify({ name: dataString["id"], total: addToTotal(dataString["price"])}));
     response.end()
   })
-}
+};
+
+let total = 0;
+const addToTotal = function (prices) {
+  const shippingCosts = 2.49;
+  for (let price in prices)
+  {
+    total += price;
+  }
+  total += shippingCosts;
+  return total;
+};
 
 const sendFile = function( response, filename ) {
-   const type = mime.getType( filename ) 
+   const type = mime.getType( filename ) ;
 
    fs.readFile( filename, function( err, content ) {
 
@@ -56,17 +62,17 @@ const sendFile = function( response, filename ) {
      if( err === null ) {
 
        // status code: https://httpstatuses.com
-       response.writeHeader( 200, { 'Content-Type': type })
+       response.writeHeader( 200, { 'Content-Type': type });
        response.end( content )
 
      }else{
 
        // file not found, error code 404
-       response.writeHeader( 404 )
+       response.writeHeader( 404 );
        response.end( '404 Error: File Not Found' )
 
      }
    })
-}
+};
 
-server.listen( process.env.PORT || port )
+server.listen( process.env.PORT || port );
