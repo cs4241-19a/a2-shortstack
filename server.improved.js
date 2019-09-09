710b3dd0950ed934153a42d7100de9bb52baadfb
Georgie <geocromwell@yahoo.com>
2019-09-09 05:55:30 -0700

    Updating all the files, see README to download project that works fully

--- server.improved.js
+++ server.improved.js
@@ -6,10 +6,11 @@ const http = require( 'http' ),
       dir  = 'public/',
       port = 3000
 
+
 const appdata = [
-  { 'player': 'Ellen Page', 'country': 'ENG', 'goals': 6, 'shots': 16, 'percentage': 0.5 },
-  { 'player': 'Megan Rapinoe', 'country': 'USA', 'goals': 6, 'shots': 12, 'percentage': .3 },
-  { 'player': 'Alex Morgan', 'country': 'USA', 'goals': 6, 'shots': 18, 'percentage': .2} 
+  { 'playerName': 'Ellen Page', 'country': 'ENG', 'goals': 6, 'shots': 16, 'goalPercentage': 0.5 },
+  { 'playerName': 'Megan Rapinoe', 'country': 'USA', 'goals': 6, 'shots': 12, 'goalPercentage': .3 },
+  { 'playerName': 'Alex Morgan', 'country': 'USA', 'goals': 6, 'shots': 18, 'goalPercentage': .2} 
 ]
 
 const server = http.createServer( function( request,response ) {
@@ -25,6 +26,8 @@ const handleGet = function( request, response ) {
 
   if( request.url === '/' ) {
     sendFile( response, 'public/index.html' )
+  }if(request.url === '/getResults'){
+    sendResults( response)
   }else{
     sendFile( response, filename )
   }
@@ -38,26 +41,31 @@ const handlePost = function( request, response ) {
   })
 
   request.on( 'end', function() {
-    console.log( JSON.parse( dataString ) )
+    let addedData =  JSON.parse( dataString )
+    if(request.url == '/submit'){
+      //here call calculate goal percentage and return it!!!!
+      let newGoalPercentage = calculateGoalPercentage(addedData.goals, addedData.shots).toString().slice(0, 4)
 
-    // ... do something with the data here!!!
-    const newPlayerInput = JSON.parse( dataString );
-
-    let goalPercentage = calculateGoalPercentage(newPlayerInput.shots, newPlayerInput.goals);
-    let newData = {
-      'goalPercent': goalPercentage
-    };
-
-    let newDatas = JSON.stringify(newData);
-
-    console.log(goalPercentage);
-
-    response.writeHead( 200, "OK", {'Content-Type': 'text/plain' })
-    response.write(newDatas)//////////////////////////////////////help
+      appdata.push({'playerName': addedData.playerName,'country': addedData.country, 'goals': addedData.goals,'shots': addedData.shots,
+                     'goalPercentage' : newGoalPercentage
+                    })
+    }
+    //send back newly added info in response for debugging purposes and to display message to user
+    const newData = {
+      'playerName': addedData.playerName,
+      'country': addedData.country,
+      'goals': addedData.goals,
+      'shots': addedData.shots,
+    }
+    const responseData = JSON.stringify(newData)    
+    response.writeHead(200, responseData,  {'Content-Type': 'text/plain' })
+    //write to response to send data back
+    response.write(responseData)
     response.end()
   })
 }
 
+//didn't change from starter code
 const sendFile = function( response, filename ) {
    const type = mime.getType( filename ) 
 
@@ -66,6 +74,7 @@ const sendFile = function( response, filename ) {
      // if the error = null, then we've loaded the file successfully
      if( err === null ) {
 
+       
        // status code: https://httpstatuses.com
        response.writeHeader( 200, { 'Content-Type': type })
        response.end( content )
@@ -80,9 +89,17 @@ const sendFile = function( response, filename ) {
    })
 }
 
-//ADDING MY CODE NOW
-const calculateGoalPercentage = function(numberOfShots, numberOfGoals){
-  return (numberOfGoals/numberOfShots);
+//method to send all the data to the results page
+const sendResults = function( response) {
+       const responseData = JSON.stringify(appdata)
+       response.writeHeader( 200, { 'Content-Type': 'plain/text' })
+       response.write(responseData)
+       response.end()
+}
+
+//method to calculate the extra column by calculating the goal percentage from shots
+const calculateGoalPercentage = function(goals, shots){
+  return goals/shots
 }
 
 server.listen( process.env.PORT || port )
