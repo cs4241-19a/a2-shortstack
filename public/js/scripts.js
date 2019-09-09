90ed4a459783fd12ac4091fbbdf45a8a05895346
Glitch (a2-brandon-m-navarro-brandon-navarro) <none>
2019-09-09 14:26:44 +0000

    Ready for submission

--- public/js/scripts.js
+++ public/js/scripts.js
@@ -1,3 +1,68 @@
 // Add some Javascript code here, to run on the front end.
 
-console.log("Welcome to assignment 2!")
+const displayTasks = function(taskData) {
+  const template = '<tr><td>{name}</td><td>{task}</td><td>{priority}</td></tr>';
+  const row = template.replace("{name}", taskData.name).replace("{task}", taskData.task).replace("{priority}", taskData.priority);
+  const tbody = document.querySelector("#all-tasks");
+  tbody.innerHTML += row;
+}
+
+const displayData = function ( data ) {
+	document.querySelector("#all-tasks").innerHTML = ""
+	for (let i = 0; i < data.length; i++) {
+	  const taskData = data[i]
+	    displayTasks(taskData)
+	}
+}
+	
+// Fetch the appdata and then display
+const loadData = function( e ) {
+	fetch( '/todo', {
+	  method:'GET',
+	})
+	.then( function( response ) {
+	  response.json().then(displayData)
+	  console.log(response)
+	})
+}
+	
+// Add task function
+const addTask = function(e) {
+  // Validate that the user has entered some input
+  if(document.getElementById('name').value === '' ||
+     document.getElementById('task').value === '' ||
+     document.getElementById('priority').value === '') {
+    return false;
+  }
+  const newTask = {
+	        name: document.getElementById('name').value,
+	        task: document.getElementById('task').value,
+	        priority:document.getElementById('priority').value,
+  };
+	
+	const body = JSON.stringify( newTask );
+  fetch( '/addTask', {
+	        method:'POST',
+	        body
+	}).then( function( response ) {
+	        resetForm();
+	        loadData();
+	})
+  resetForm();
+  loadData();
+	return false;
+}
+      
+// Reset form after adding task
+const resetForm = () => {
+  document.getElementById('name').value = '';
+  document.getElementById('task').value = '';
+  document.getElementById('priority').value = '';
+};
+
+// Startup function
+window.onload = function() {
+	    const addTaskbutton = document.getElementById( 'submit-btn' )
+	    addTaskbutton.onclick = addTask
+	    loadData()
+}
