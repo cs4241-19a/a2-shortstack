9c1c4fcecf87b986e65e8f782a435f082017f299
Glitch (winnycheng-a2-shortstack) <none>
2019-09-09 00:28:16 +0000

    🍭💳 Checkpoint ./public/js/draw.js:1011354/2320

--- public/js/draw.js
+++ public/js/draw.js
@@ -27,64 +27,16 @@ var width;
 
 var x, y, z;  //x, y, z, values of the center of object
 
-//Read the inputted file
-function readFile(event) {
-    //check to see if browser supports FileReader
-    if (window.FileReader) {
-        var reader = new FileReader();
-        reader.onload = parseFile;
-        reader.readAsText(event.target.files[0]);
-    } else
-        alert('Browser does not support FileReader');
-}
-
-//parse file data and store values in appropriate variables
-function parseFile(event){
+//set vertices and polygons
+function intialize(numVer, ver, numPoly, verIdx){
 
     reset();  //ensure no value from previous read file remains in variables
-
-    var plyFile = event.target.result;
-    var plyLines = plyFile.split(/\r\n|\n/g);
-
-    //check for "ply" in file, if not present exit
-    if(!plyLines[0].toLowerCase().includes("ply")){
-        alert("Sorry, File cannot be read. Please check your file and try again.");
-        return 1;
-    }
-
-    var end = false;  //check for end of header
-    var line;         //temp variable to a single line
-    var ele;          //each element in a line
-    var counter = 0;
-
-    //iterate through each line of the file
-    for(var i = 1; i < plyLines.length; i++) {
-        line = plyLines[i].toLowerCase();
-        ele = line.split(" ").filter(Boolean);
-
-        if(line.includes("element")){      //read value from element lines
-            if(line.includes("vertex"))
-                numVertices = parseFloat(ele[ele.length - 1]);
-            else if(line.includes("face"))
-                numPolygons = parseFloat(ele[ele.length -1]);
-        }
-        else if(line.includes("end_header")) {
-            end = true;
-        }
-        else if(end && !(line === "")){     //end of header, parse all vertices and indexes
-            if(counter < numVertices){
-                counter++;
-                vertices.push(vec4(parseFloat(ele[0]), parseFloat(ele[1]), parseFloat(ele[2]), 1.0))
-            }
-            else if(counter < numVertices + numPolygons){
-                counter++;
-                vertexIndx.push(parseFloat(ele[1]));
-                vertexIndx.push(parseFloat(ele[2]));
-                vertexIndx.push(parseFloat(ele[3]));
-            }
-        }
-    }
-
+    
+    numVertices = numVer;
+    numPolygons = numPoly;
+    vertices = ver;
+    vertexIndx = verIdx;
+  
     for(var p = 0; p < numPolygons; p++)
         calcNormal(p);
 
@@ -110,8 +62,7 @@ function draw(){
 
     // Get the rendering context for WebGL
     gl = WebGLUtils.setupWebGL(canvas, undefined);
-    if (!gl)
-    {
+    if (!gl) {
         console.log('Failed to get the rendering context for WebGL');
         return;
     }
