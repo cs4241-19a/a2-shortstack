4031400d62a2b72b84357469c105a61617f5c76f
Kreiselman <jkreiselman@wpi.edu>
2019-09-08 22:00:20 -0400

    import from glitch

--- public/js/scripts.js
+++ public/js/scripts.js
@@ -1,3 +1,61 @@
-// Add some Javascript code here, to run on the front end.
+function submit(  ) {
 
-console.log("Welcome to assignment 2!")
+    console.log("Testing for name " + document.getElementById('yourname') )
+    document.getElementById('info').submit()
+    setTimeout(function(){
+        clearTable()
+        getData()
+    },1000)
+
+
+
+
+    return false
+}
+
+function onDelete(){
+    document.getElementById('delete').submit()
+    setTimeout(function(){
+        clearTable()
+        document.getElementById("dataTable").deleteRow(1)
+        getData()
+    },1000)
+
+    return false
+}
+
+
+
+
+
+function clearTable()
+{
+
+    $.get('/data',function(response){
+
+        response.forEach(function(item){
+            console.log("cleariunug list" )
+            document.getElementById("dataTable").deleteRow(1)
+        })
+
+    })
+
+    console.log("Table has been reset")
+}
+
+function getData()
+{
+    $.get('/data',function(response){
+
+
+        response.forEach(function(item){
+            console.log("item is " +item)
+            $('#dataTable').append('<tr><td>' + item.name + '</td><td>' + item.year + '</td><td>' + item.inches  + '</td><td>' + item.cm + '</td></tr>')
+            console.log("wrote " + item.name)
+        })
+
+
+
+    })
+    console.log("Done writing")
+}
--- server.improved.js
+++ server.improved.js
@@ -1,72 +1,119 @@
 const http = require( 'http' ),
-      fs   = require( 'fs' ),
-      // IMPORTANT: you must run `npm install` in the directory for this assignment
-      // to install the mime library used in the following line of code
-      mime = require( 'mime' ),
-      dir  = 'public/',
-      port = 3000
+    fs   = require( 'fs' ),
+    // IMPORTANT: you must run `npm install` in the directory for this assignment
+    // to install the mime library used in the following line of code
+    mime = require( 'mime' ),
+    dir  = 'public/',
+    port = 3000,
+    Express = require('express'),
+    app = Express(),
+    bodyParser = require('body-parser')
+app.use(Express.static('public'))
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded())
 
 const appdata = [
-  { 'model': 'toyota', 'year': 1999, 'mpg': 23 },
-  { 'model': 'honda', 'year': 2004, 'mpg': 30 },
-  { 'model': 'ford', 'year': 1987, 'mpg': 14} 
+  { 'name': 'Justin', 'year': 2020, 'inches': 71 },
+  { 'name': 'Bob', 'year': 2021, 'inches': 60 },
+  { 'name': 'Andy', 'year': 2022, 'inches': 84}
 ]
 
-const server = http.createServer( function( request,response ) {
-  if( request.method === 'GET' ) {
-    handleGet( request, response )    
-  }else if( request.method === 'POST' ){
-    handlePost( request, response ) 
+const newData = []
+
+
+
+
+app.get("/", function(request, response){
+  sendFile(response, 'public/index.html')
+});
+
+
+
+app.post("/submit", function(request, response){
+  console.log(request.body)
+  console.log("name is " + request.body.yourname)
+  let json = { name: request.body.yourname, year: request.body.classyear, inches: request.body.height }
+  let index = -1
+  let val = request.body.yourname
+  let filteredObj = appdata.find(function(item,i){
+    if(item.name === val){
+      index = i
+      return i
+    }
+  })
+  console.log(request.body.yourname + "is in position " + index  )
+  if(index > -1)
+  {
+    appdata.splice(index, 1)
+  }
+
+  appdata.push(json)
+  console.log(appdata)
+})
+
+
+app.post("/delete", function(request,response){
+  let json = { name: request.body.delName, year: 200, inches: 0}
+  let index = -1
+  let val = request.body.delName
+  let filteredObj = appdata.find(function(item,i){
+    if(item.name === val){
+      index = i
+      return i
+    }
+  })
+  console.log(request.body.delName + " is in position " + index  )
+  if(index > -1)
+  {
+    appdata.splice(index, 1)
   }
 })
 
-const handleGet = function( request, response ) {
-  const filename = dir + request.url.slice( 1 ) 
+app.get("/data", function(request, response){
+  console.log("Pre newData is " )
+  console.log(newData)
 
-  if( request.url === '/' ) {
-    sendFile( response, 'public/index.html' )
-  }else{
-    sendFile( response, filename )
+
+
+
+  appdata.forEach(function(item){
+
+    newData.push({name: item.name, year: item.year, inches: item.inches, cm: (item.inches * 2.54)})
+  })
+
+  while(newData.length !== appdata.length)
+  {
+    newData.shift()
   }
-}
 
-const handlePost = function( request, response ) {
-  let dataString = ''
+  console.log("Post newData is ")
+  console.log(newData)
+  console.log(appdata)
 
-  request.on( 'data', function( data ) {
-      dataString += data 
-  })
-
-  request.on( 'end', function() {
-    console.log( JSON.parse( dataString ) )
-
-    // ... do something with the data here!!!
-
-    response.writeHead( 200, "OK", {'Content-Type': 'text/plain' })
-    response.end()
-  })
-}
+  response.send(newData)
+})
 
 const sendFile = function( response, filename ) {
-   const type = mime.getType( filename ) 
+  const type = mime.getType( filename )
 
-   fs.readFile( filename, function( err, content ) {
+  fs.readFile( filename, function( err, content ) {
 
-     // if the error = null, then we've loaded the file successfully
-     if( err === null ) {
+    // if the error = null, then we've loaded the file successfully
+    if( err === null ) {
 
-       // status code: https://httpstatuses.com
-       response.writeHeader( 200, { 'Content-Type': type })
-       response.end( content )
+      // status code: https://httpstatuses.com
+      response.writeHeader( 200, { 'Content-Type': type })
+      response.end( content )
 
-     }else{
+    }else{
 
-       // file not found, error code 404
-       response.writeHeader( 404 )
-       response.end( '404 Error: File Not Found' )
+      // file not found, error code 404
+      response.writeHeader( 404 )
+      response.end( '404 Error: File Not Found' )
 
-     }
-   })
+    }
+  })
 }
 
-server.listen( process.env.PORT || port )
+
+app.listen(process.env.PORT || port)
