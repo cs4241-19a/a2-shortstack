3793233be961c0ec096f57dad100f90d3001e848
Ryc1x <jerry_huangrui@outlook.com>
2019-09-08 23:34:58 -0400

    finished a2

--- public/js/scripts.js
+++ public/js/scripts.js
@@ -1,3 +1,120 @@
-// Add some Javascript code here, to run on the front end.
+const rowItem = `<tr>
+<th scope="row"></th>
+<td>[name]</td>
+<td>[birthday]</td>
+<td>[major]</td>
+<td>[age]</td>
+<td><button type="button" class="btn btn-danger">Delete</button></td>
+</tr>
+`
 
-console.log("Welcome to assignment 2!")
+
+
+window.onload = function () {
+    const button = document.querySelector('#add-row-btn')
+    button.onclick = handleSubmitPerson
+    updateTableContent()
+    addDropdownMajors()
+    console.log('this javascript file was successfully loaded.')
+}
+
+function topFunction() {
+    document.body.scrollTop = 0; // For Safari
+    document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+}
+
+/**
+ * add a row to the table of people
+ */
+const handleSubmitPerson = function (e) {
+    // prevent default form action from being carried out
+    e.preventDefault()
+
+    const form = $('#add-form')
+    const name = form.find('#input-name').val()
+    const birthday = form.find('#input-birthday').val()
+    const major = form.find('#select-major').val()
+
+    json = {
+        name: name,
+        birthday: birthday,
+        major: major
+    }
+    body = JSON.stringify(json)
+
+    fetch('person', {
+        method: 'POST',
+        body: body
+    }).then(function (response) {
+        console.log(response)
+    })
+
+    updateTableContent();
+}
+
+
+const handleDeletePerson = function (idx) {
+    let path = 'person?idx=' + idx;
+
+    fetch(path, {
+        method: 'DELETE'
+    }).then(function (response) {
+        console.log(response)
+    })
+    
+    updateTableContent();
+}
+
+/**
+ * Add the json/major.json to the dropdown box
+ */
+const addDropdownMajors = function () {
+    $.getJSON("json/major.json", function (data) {
+        majors = data.majors
+        $.each(majors, function (key, val) {
+            $('#select-major').append("<option value='" + val + "'>" + val + "</option>");
+        });
+    });
+}
+
+/**
+ * Clear table, fetch data from server, then place people on table
+ */
+const updateTableContent = function () {
+    // clear the table
+    $('#people-table').find('tbody').empty()
+
+    // fetch data from the server
+    fetch('people', {
+        method: 'GET'
+    }).then(function (response) {
+        console.log(response)
+        return response.json()
+    }).then(function (data) {
+        console.log(data)
+        $.each(data, function (idx, person) {
+            const tbody = $('#people-table').find('tbody')
+            tbody.append(
+                $('<tr>')
+                    .append($('<th>')
+                        .attr('scope', 'row')
+                        .text(idx + 1))
+                    .append($('<td>')
+                        .text(person.name))
+                    .append($('<td>')
+                        .text(person.birthday))
+                    .append($('<td>')
+                        .text(person.major))
+                    .append($('<td>')
+                        .text(person.age))
+                    .append($('<td>')
+                        .append($('<button>')
+                            .attr('type', 'button')
+                            .attr('class', 'btn btn-danger')
+                            .attr('onclick', 'handleDeletePerson(' + idx + ')')
+                            .text('Delete')))
+            );
+        })
+        return data
+    })
+}
--- server.improved.js
+++ server.improved.js
@@ -1,36 +1,44 @@
 const http = require('http'),
     fs = require('fs'),
+    url = require('url'),
     // IMPORTANT: you must run `npm install` in the directory for this assignment
     // to install the mime library used in the following line of code
     mime = require('mime'),
     dir = 'public/',
     port = 3000
 
-const appdata = [
-    { 'model': 'toyota', 'year': 1999, 'mpg': 23 },
-    { 'model': 'honda', 'year': 2004, 'mpg': 30 },
-    { 'model': 'ford', 'year': 1987, 'mpg': 14 }
-]
+let peopleData = JSON.parse(fs.readFileSync('public/json/people.json'));
 
 const server = http.createServer(function (request, response) {
     if (request.method === 'GET') {
         handleGet(request, response)
     } else if (request.method === 'POST') {
         handlePost(request, response)
+    } else if (request.method === 'DELETE') {
+        handleDelete(request, response)
+    } else {
+        response.writeHeader(404);
+        response.end('404 Error: Resource Not Found');
     }
 })
 
 const handleGet = function (request, response) {
-    const filename = dir + request.url.slice(1)
+    console.log('GET: ', request.url)
+
+    const filename = dir + request.url
 
     if (request.url === '/') {
         sendFile(response, 'public/index.html')
+    } else if (request.url === '/people') {
+        response.writeHeader(200, { 'Content-Type': 'application/json' })
+        response.end(JSON.stringify(peopleData))
     } else {
         sendFile(response, filename)
     }
 }
 
 const handlePost = function (request, response) {
+    console.log('POST: ', request.url)
     let dataString = ''
 
     request.on('data', function (data) {
@@ -38,14 +46,38 @@ const handlePost = function (request, response) {
     })
 
     request.on('end', function () {
-        console.log(JSON.parse(dataString))
-
-        // ... do something with the data here!!!
-
-        response.writeHead(200, "OK", { 'Content-Type': 'text/plain' })
-        response.end()
+        data = JSON.parse(dataString)
+        console.log(data)
+        switch (request.url) {
+            case '/person':
+                response.writeHeader(200, { 'Content-Type': 'text/plain' });
+                data.age = _calculateAge(data.birthday);
+                console.log(data)
+                peopleData.push(data);
+                response.end('200 Success: The person is recorded');
+                break;
+            default:
+                response.writeHeader(404);
+                response.end('404 Error: Resource Not Found');
+                break;
+        }
     })
 }
+
+const handleDelete = function (request, response) {
+    console.log('DELETE: ', request.url)
+    const parsed_url = url.parse(request.url, true)
+    if (parsed_url.pathname === '/person') {
+        let idx = parsed_url.query.idx
+        peopleData.splice(idx, 1)
+        response.writeHeader(200, { 'Content-Type': 'Text/plain' });
+        response.end('200 Success: The person is deleted');
+    } else {
+        response.writeHeader(404);
+        response.end('404 Error: Resource Not Found');
+    }
+}
+
 
 const sendFile = function (response, filename) {
     const type = mime.getType(filename)
@@ -69,4 +101,14 @@ const sendFile = function (response, filename) {
     })
 }
 
+const _calculateAge = function (birthday_str) { // birthday is a date
+    var birthday = new Date(birthday_str)
+    var ageDifMs = Date.now() - birthday.getTime();
+    var ageDate = new Date(ageDifMs); // miliseconds from epoch
+    return Math.abs(ageDate.getUTCFullYear() - 1970);
+}
+
 server.listen(process.env.PORT || port)
+
+console.log('Served at localhost:3000')
+console.log('Ctrl-c to quit')
