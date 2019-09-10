a608e9fc890d0522c2df460ddb6d10794bb68656
sdandeneau10 <spdandeneau@wpi.edu>
2019-09-09 22:26:41 -0400

    Final

--- server.improved.js
+++ server.improved.js
@@ -6,11 +6,6 @@ const http = require( 'http' ),
       dir  = 'public/',
       port = 3000
 
-const appdata = [
-  { 'model': 'toyota', 'year': 1999, 'mpg': 23 },
-  { 'model': 'honda', 'year': 2004, 'mpg': 30 },
-  { 'model': 'ford', 'year': 1987, 'mpg': 14} 
-]
 
 const server = http.createServer( function( request,response ) {
   if( request.method === 'GET' ) {
@@ -40,8 +35,6 @@ const handlePost = function( request, response ) {
   request.on( 'end', function() {
     console.log( JSON.parse( dataString ) )
 
-    // ... do something with the data here!!!
-
     response.writeHead( 200, "OK", {'Content-Type': 'text/plain' })
     response.end()
   })
@@ -68,5 +61,6 @@ const sendFile = function( response, filename ) {
      }
    })
 }
+
 
 server.listen( process.env.PORT || port )
