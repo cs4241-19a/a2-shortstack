39d8c8f60024a633bb819beb2922b2324dbc2233
Glitch (paristhecity-a3-paris-lopez) <none>
2019-09-15 19:13:26 +0000

    🛍🎓 Checkpoint ./app.js:6266626/3018 ./package.json:6266626/15

--- app.js
+++ app.js
@@ -1,86 +1,23 @@
-const app = require('../app');
-const debug = require('debug')('app:server');
-const http = require('http');
+const express = require('express')
+const app = express()
+const port = 3000
+var bodyParser = require('body-parser');
+var passport = require('passport');
+var LocalStrategy = require('passport-local').Strategy;
 
 
-const createError = require('http-errors');
-const express = require('express');
-const path = require('path');
-const cookieParser = require('cookie-parser');
-const logger = require('morgan');
-
-const indexRouter = require('./routes/index');
-const usersRouter = require('./routes/users');
-const bodyParser = require('body-parser');
-
-const low = require('lowdb');
-const FileSync = require('lowdb/adapters/FileSync');
-
-const adapter = new FileSync('db.json');
-const db = low(adapter);
-
-const app = express();
-const passport = require('passport');
-const LocalStrategy = require('passport-local').Strategy
-
-// password information
-app.use(passport.initialize());
-app.use(passport.session());
-
-app.get('/success', (req, res) => res.send("Welcome "+req.query.username+"!!"));
-app.get('/error', (req, res) => res.send("error logging in"));
-
-passport.serializeUser(function(user, cb) {
-  cb(null, user.id);
-});
-
-passport.deserializeUser(function(id, cb) {
-  User.findById(id, function(err, user) {
-    cb(err, user);
-  });
-});
-
-// database information
-db.defaults({ posts: [], user: {} })
-  .value()
-
-db.get('posts')
-  .push({ id: 1, title: 'lowdb is awesome'})
-  .value()
-
-db.set('user.name', 'typicode')
-  .value()
+app.use(express.static('public'))
 
 
+passport.use(new LocalStrategy( 
+  function(username, password, done) {
+    User.findOne({ username: username }, function (err, user) {
+      if (err) { return done(err); }
+      if (!user) { return done(null, false); }
+      if (!user.verifyPassword(password)) { return done(null, false); }
+      return done(null, user);
+    });
+  }
+));
 
-
-app.use(bodyParser.urlencoded({ extended: true }));
-app.get('/', (req, res) => res.sendFile('index.html', { root : 'public'}));
-
-// view engine setup
-app.use(logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
-
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  next(createError(404));
-});
-
-// error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
-
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
-});
-
-module.exports = app;
-
-
-//app.listen(port , () => console.log('App listening on port ' + port));
+app.listen(port, () => console.log(`Example app listening on port ${port}!`))
