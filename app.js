8d9d694fa1cfd03e670d8ebce82ee0c8e4735f5d
Glitch (paristhecity-a3-paris-lopez) <none>
2019-09-15 19:25:18 +0000

    👲🎃 Checkpoint ./app.js:6266626/338

--- app.js
+++ app.js
@@ -1,14 +1,19 @@
 const express = require('express')
 const app = express()
 const port = 3000
-var bodyParser = require('body-parser');
-var passport = require('passport');
-var LocalStrategy = require('passport-local').Strategy;
 
+const bodyParser = require('body-parser');
+const passport = require('passport');
+const LocalStrategy = require('passport-local').Strategy;
+
+const low = require('lowdb')
+const FileSync = require('lowdb/adapters/FileSync')
+const adapter = new FileSync('db.json')
+const db = low(adapter)
 
 app.use(express.static('public'))
 
-
+//password
 passport.use(new LocalStrategy( 
   function(username, password, done) {
     User.findOne({ username: username }, function (err, user) {
@@ -19,5 +24,17 @@ passport.use(new LocalStrategy(
     });
   }
 ));
+
+//database
+db.defaults({ posts: [] })
+  .write()
+
+const result = db.get('posts')
+  .push({ title: process.argv[2] })
+  .write()
+
+console.log(result)
+
+
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
