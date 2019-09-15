8fa500da8bebfc1210c34b3f450a893d7d9b44e4
Glitch (paristhecity-a3-paris-lopez) <none>
2019-09-15 22:33:52 +0000

    🎅💑 Checkpoint ./server.js:6266626/1036

--- server.js
+++ server.js
@@ -7,6 +7,7 @@ const port = 3000
 //passport imports
 const passport = require('passport');
 const LocalStrategy = require('passport-local').Strategy;
+const session = require('express-session');
 
 //lowdb imports
 const low = require('lowdb')
@@ -15,11 +16,44 @@ const adapter = new FileSync('db.json')
 const db = low(adapter);
 
 app.use(express.static('public'));
-app.use(bodyParser.json());
+//app.use(bodyParser.json());
 
 //DATABASE
 db.defaults({ users: [] }).write();
+let users = []
+var i;
+for(i = 0; i > -1; i++) {
+  let user = db.get(users[$i])
+}
+  
 
+
+//Password Authentication
+passport.serializeUser((user, done) => done(null, user.username))
+passport.deserializeUser((username, done) => {
+    const user = users.find(u => u.username === username)
+    if (user !== undefined) {
+        done(null, user)
+    } else {
+        done(null, false, { message: 'user not found; session not restored' })
+    }
+})
+app.use(session({ secret: 'secret', resave: false, saveUninitialized: false }))
+app.use(passport.initialize())
+app.use(passport.session())
+app.post('/test', function(req, res) {
+    console.log('authenticate with cookie?', req.user)
+    res.json({ status: 'success' })
+})
+
+app.post(
+    '/login',
+    passport.authenticate('local'),
+    function(req, res) {
+        you = allUsers.find(__user => __user.username === req.user.username)
+        res.json({ status: true })
+    }
+)
 
 
 
