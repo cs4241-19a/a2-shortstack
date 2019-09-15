aa5b981c7f3b8b2525ca5f80093c959d0b7cb744
Glitch (a2-yicheng-yang) <none>
2019-09-15 20:45:22 +0000

    💈⛩ Checkpoint ./server.improved.js:6271330/401

--- server.improved.js
+++ server.improved.js
@@ -1,27 +1,21 @@
 const express    = require('express'),
       app        = express(),
-      bodyparser = require( 'body-parser' ),
-      dreams     = []
-      // favicon = require('serve-favicon'),
-      // path = require('path')
+      // bodyparser = require( 'body-parser' ),
+      // dreams     = [],
+      favicon = require('serve-favicon'),
+      path = require('path'),
+      passport = require('passport'),
+      low = require('lowdb'),
+      db = low(adapter),
 
 // automatically deliver all files in the public folder
 // with the correct headers / MIME type.
 app.use( express.static( 'public' ) )
 //app.use(favicon(path.join(__dirname, 'assets', 'icon.ico')))
-
-// get json when appropriate
-app.use( bodyparser.json() )
-
 // even with our static file handler, we still
 // need to explicitly handle the domain name alone...
 
 
-app.post( '/submit', function( request, response ) {
-  dreams.push( request.body.newdream )
-  response.writeHead( 200, { 'Content-Type': 'application/json'})
-  response.end( JSON.stringify( dreams ) )
-})
 
 const low = require('lowdb')
 const FileSync = require('lowdb/adapters/FileSync')
