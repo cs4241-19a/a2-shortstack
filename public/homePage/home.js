da38e0af488a174c68d585a5b0d2d461ea101161
Glitch (paristhecity-a3-paris-lopez) <none>
2019-09-16 03:05:56 +0000

    🔑🍪 Checkpoint ./public/index.css:6266626/42 ./public/index.html:6266626/975 ./public/homePage/home.html:6266626/147 ./public/homePage/home.js:6266626/1050

--- public/homePage/home.js
+++ public/homePage/home.js
@@ -1,4 +1,36 @@
+
+const loginAction = function( e ) {
+    e.preventDefault()
+  
+    const username = document.querySelector('#first')
+    const password = document.querySelector('#last')
+    
+    const body = { username: username.value, password: password.value };
+    console.log(body);
+    fetch('/login', {
+            method: 'POST',
+            body : JSON.stringify(body),
+            headers: { 'Content-Type': 'application/json' }
+          })
+          .then( response => {
+                console.log(response)
+                return response.json()
+          })
+          .then( response => {
+              console.log(response)
+              location.href = './homePage/home.html'
+          })
+          .catch(err => {
+              console.log(err)
+              state.innerHTML = "Incorrect Username or Password";
+          })
+      return false;
+};
+
+
 window.onload = function() {
-    console.log("home.html: javascript loading")
+  console.log("home.html: javascript loading")
+  
+  const addButton = querySelector()
 
 }
