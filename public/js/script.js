c7114bdc1a3a349f1c84e87aaaadc5851acc06f4
Luke Bodwell <ljbodwell@wpi.edu>
2020-09-09 01:05:47 -0400

    Implement ability to display table of data obtained from server.

--- public/js/script.js
+++ public/js/script.js
@@ -1,15 +1,70 @@
+"use strict";
+
+const submitButton = document.getElementById("submit-button");
+
+const updateData = async () => {
+	fetch("/api/data", {
+		method: "GET",
+	}).then((res) => {
+		return res.json();
+	}).then((data) => {
+		formatDataAsTable(data);
+	});
+}
+
+window.addEventListener("load", updateData);
+
+window.setInterval(updateData, 10000);
+
 document.getElementById("submit-button").addEventListener("click", (evt) => {
 	evt.preventDefault();
 
-	const body = JSON.stringify({name: document.getElementById("f-name").value + " " + document.getElementById("l-name").value});
+	const fNameField = document.getElementById("f-name");
+	const lNameField = document.getElementById("l-name");
+	const body = JSON.stringify({name: `${fNameField.value} ${lNameField.value}`, test: "testing"});
 
 	fetch("/submit", {
 		method: "POST",
 		body,
 	}).then((res) => {
 		console.log(res);
-		window.location.replace(window.location.href);
+		updateData();
+		fNameField.value = "";
+		lNameField.value = "";
 	});
 
 	return false;
 });
+ 
+const formatDataAsTable = (data) => {
+	let keys = [];
+
+	data.forEach(row => {
+		for (let key in row) {
+			if (!keys.includes(key)) {
+				keys.push(key);
+			}
+		}
+	});
+	
+	const table = document.createElement("table");
+	let tableRow = table.insertRow(-1);
+
+	keys.forEach(key => {
+		let tableHeader = document.createElement("th");
+		tableHeader.innerHTML = key.slice(0, 1).toUpperCase() + key.slice(1);
+		tableRow.appendChild(tableHeader);
+	});
+
+	data.forEach(row => {
+		tableRow = table.insertRow(-1);
+		keys.forEach(key => {
+			let cell = tableRow.insertCell(-1);
+			cell.innerHTML = row[key];
+		});
+	});
+
+	const dataContainer = document.getElementById("data-container");
+	dataContainer.innerHTML = "";
+	dataContainer.appendChild(table);
+}
--- server.js
+++ server.js
@@ -1,37 +1,38 @@
 const http = require("http");
 const fs = require("fs");
 const mime = require("mime");
-const dir = "public/";
+const {v4: uuid} = require("uuid");
+
+const PUBLIC_DIR = "./public";
 const port = 3000;
 
 const appData = [
-	{name: "Test Testerson"},
+	{id: uuid(), name: "Test Testerson"},
 ];
 
 http.createServer((req, res) => {
 	if (req.method === "GET") {
-		const file = req.url === "/" ? "public/index.html" : dir + req.url.slice(1);	
-
-		fs.readFile(file, (err, content) => {
-			if (err) {
-				res.writeHeader(404);
-				res.end("Error 404: File Not Found.");
-			} else {
+		if (req.url === "/api/data") {
+			res.writeHeader(200, {"Content-Type": "application/json"});
+			res.end(JSON.stringify(appData));
+		} else {
+			const file = req.url === "/" ? PUBLIC_DIR + "/index.html" : PUBLIC_DIR + req.url;
+			fs.readFile(file, (err, content) => {
+				if (err) {
+					res.writeHeader(404);
+					res.end("Error 404: File Not Found.");
+					return;
+				}
 				res.writeHeader(200, {"Content-Type": mime.getType(file)});
 				res.end(content);
-			}
-		});
+			});
+		}
 	} else if (req.method === "POST") {
 		let dataString = "";
-
 		req.on("data", (data) => dataString += data);
-
 		req.on("end", () => {
 			appData.push(JSON.parse(dataString));
-			console.log("App data:");
-			console.log(appData);
-
-			// ... do something with the data here!!!
+			console.log("App data: " + appData); 
 
 			res.writeHead(200, "OK", {"Content-Type": "text/plain"});
 			res.end();
