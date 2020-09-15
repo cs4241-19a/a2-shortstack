3f0781dacb0d6f1add1fbbc412f53de367a2f520
rkayastha98 <rkayastha@wpi.edu>
2020-09-15 00:36:10 -0400

    First commit

--- public/js/scripts.js
+++ public/js/scripts.js
@@ -1,3 +1,30 @@
 // Add some Javascript code here, to run on the front end.
 
 console.log("Welcome to assignment 2!")
+
+var addBox=document.getElementById("addBox")
+var cal=document.getElementById("cal")
+var hist=document.getElementById("hist")
+var today=document.getElementById("today")
+
+document.getElementById("addBtn").addEventListener("click",function () {
+    if (addBox.style.visibility === 'hidden') {
+        addBox.style.visibility = 'visible';
+        const button = document.querySelector( '#submit' )
+          button.onclick = submit
+    } else {
+        addBox.style.visibility = 'hidden';
+    }
+
+})
+
+document.getElementById("history").addEventListener("click",function () {
+    hist.style.display = 'inline';
+    cal.style.display="none";
+
+})
+
+document.getElementById("today").addEventListener("click", function () {
+    cal.style.display='inline'
+    hist.style.display='none'
+})
--- server.improved.js
+++ server.improved.js
@@ -1,27 +1,22 @@
 const http = require( 'http' ),
-      fs   = require( 'fs' ),
-      // IMPORTANT: you must run `npm install` in the directory for this assignment
-      // to install the mime library used in the following line of code
-      mime = require( 'mime' ),
-      dir  = 'public/',
-      port = 3000
+    fs   = require( 'fs' ),
+    // IMPORTANT: you must run `npm install` in the directory for this assignment
+    // to install the mime library used in the following line of code
+    mime = require( 'mime' ),
+    dir  = 'public/',
+    port = 3000
 
-const appdata = [
-  { 'model': 'toyota', 'year': 1999, 'mpg': 23 },
-  { 'model': 'honda', 'year': 2004, 'mpg': 30 },
-  { 'model': 'ford', 'year': 1987, 'mpg': 14} 
-]
 
 const server = http.createServer( function( request,response ) {
   if( request.method === 'GET' ) {
-    handleGet( request, response )    
+    handleGet( request, response )
   }else if( request.method === 'POST' ){
-    handlePost( request, response ) 
+    handlePost( request, response )
   }
 })
 
 const handleGet = function( request, response ) {
-  const filename = dir + request.url.slice( 1 ) 
+  const filename = dir + request.url.slice( 1 )
 
   if( request.url === '/' ) {
     sendFile( response, 'public/index.html' )
@@ -34,7 +29,7 @@ const handlePost = function( request, response ) {
   let dataString = ''
 
   request.on( 'data', function( data ) {
-      dataString += data 
+    dataString += data
   })
 
   request.on( 'end', function() {
@@ -48,25 +43,25 @@ const handlePost = function( request, response ) {
 }
 
 const sendFile = function( response, filename ) {
-   const type = mime.getType( filename ) 
+  const type = mime.getType( filename )
 
-   fs.readFile( filename, function( err, content ) {
+  fs.readFile( filename, function( err, content ) {
 
-     // if the error = null, then we've loaded the file successfully
-     if( err === null ) {
+    // if the error = null, then we've loaded the file successfully
+    if( err === null ) {
 
-       // status code: https://httpstatuses.com
-       response.writeHeader( 200, { 'Content-Type': type })
-       response.end( content )
+      // status code: https://httpstatuses.com
+      response.writeHeader( 200, { 'Content-Type': type })
+      response.end( content )
 
-     }else{
+    }else{
 
-       // file not found, error code 404
-       response.writeHeader( 404 )
-       response.end( '404 Error: File Not Found' )
+      // file not found, error code 404
+      response.writeHeader( 404 )
+      response.end( '404 Error: File Not Found' )
 
-     }
-   })
+    }
+  })
 }
 
 server.listen( process.env.PORT || port )
