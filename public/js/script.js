e8fe8c57227f77222c25f280befecd98c0d8ae97
Luke Bodwell <ljbodwell@wpi.edu>
2020-09-16 06:37:32 -0400

    Update data model. Add ability to delete data. Styling improvements.

--- public/js/script.js
+++ public/js/script.js
@@ -1,8 +1,9 @@
 "use strict";
 
 const updateData = async () => {
-	const res = await fetch("/api/data", {method: "GET"});
-	formatDataAsTable(await res.json());
+	const res = await fetch("/api/users", {method: "GET"});
+	const data = await res.json();
+	formatDataAsTable(data);
 }
 
 window.addEventListener("load", updateData);
@@ -11,14 +12,21 @@ window.setInterval(updateData, 10000);
 document.getElementById("submit-button").addEventListener("click", async (evt) => {
 	evt.preventDefault();
 
-	const fNameField = document.getElementById("f-name");
-	const lNameField = document.getElementById("l-name");
+	const fName = document.getElementById("f-name");
+	const lName = document.getElementById("l-name");
+	const email = document.getElementById("email");
+	const dob = document.getElementById("dob");
+	const body = JSON.stringify({name: `${fName.value} ${lName.value}`, email: email.value, dob: dob.value});
 
-	const body = JSON.stringify({name: `${fNameField.value} ${lNameField.value}`});
-	await fetch("/submit", {method: "POST", body});
-	updateData();
-	fNameField.value = "";
-	lNameField.value = "";
+	const res = await fetch("/api/users", {method: "POST", body});
+	if (res) {
+		const data = await res.json();
+		formatDataAsTable(data);
+		fName.value = "";
+		lName.value = "";
+		email.value = "";
+		dob.value = "";
+	}
 
 	return false;
 });
@@ -39,20 +47,81 @@ const formatDataAsTable = (data) => {
 	let tableRow = table.insertRow(-1);
 
 	keys.forEach(key => {
-		let tableHeader = document.createElement("th");
+		const tableHeader = document.createElement("th");
 		tableHeader.innerHTML = key.slice(0, 1).toUpperCase() + key.slice(1);
 		tableRow.appendChild(tableHeader);
 	});
 
+	if (keys.length > 0) {
+		const editHeader = document.createElement("th");
+		editHeader.innerHTML = "Edit";
+		tableRow.appendChild(editHeader);
+
+		const deleteHeader = document.createElement("th");
+		deleteHeader.innerHTML = "Delete";
+		tableRow.appendChild(deleteHeader);
+	}
+
 	data.forEach(row => {
 		tableRow = table.insertRow(-1);
 		keys.forEach(key => {
-			let cell = tableRow.insertCell(-1);
-			cell.innerHTML = row[key];
+			const cell = tableRow.insertCell(-1);
+			const value = row[key];
+			cell.innerHTML = value != null ? value : "null";
 		});
+		
+		if (keys.length > 0) {
+			const editCell = tableRow.insertCell(-1);
+			editCell.style="text-align: center"
+			const editButton = document.createElement("input");
+			editButton.type = "button";
+			editButton.className = "btn btn-secondary";
+			editButton.value = "Edit";
+			editButton.style="border-radius: 8px";
+			editCell.appendChild(editButton);
+			editButton.onclick = () => editUser(data[editCell.parentNode.rowIndex - 1]);
+			
+			const deleteCell = tableRow.insertCell(-1);
+			deleteCell.style="text-align: center"
+			const deleteButton = document.createElement("input");
+			deleteButton.type = "button";
+			deleteButton.className = "btn btn-danger";
+			deleteButton.value = "X";
+			deleteButton.style="border-radius: 8px";
+			deleteCell.appendChild(deleteButton);
+			deleteButton.onclick = () => deleteUser(data[deleteCell.parentNode.rowIndex - 1]);
+		}
 	});
 
 	const dataContainer = document.getElementById("data-container");
-	dataContainer.innerHTML = "";
-	dataContainer.appendChild(table);
+	if (keys.length > 0) {
+		dataContainer.innerHTML = "";
+		dataContainer.appendChild(table);
+	} else {
+		dataContainer.innerHTML = "No users";
+	}
+}
+
+const editUser = async ({id}) => {
+	if (id) {
+		const params = new URLSearchParams({id});
+		//TODO
+		const body = null;
+		const res = await fetch(`/api/users?${params}`, {method: "PATCH"}, body);
+		if (res) {
+			const data = await res.json();
+			formatDataAsTable(data);
+		}
+	}
+}
+
+const deleteUser = async ({id}) => {
+	if (id) {
+		const params = new URLSearchParams({id});
+		const res = await fetch(`/api/users?${params}`, {method: "DELETE"});
+		if (res) {
+			const data = await res.json();
+			formatDataAsTable(data);
+		}
+	}
 }
--- server.js
+++ server.js
@@ -1,45 +1,126 @@
 const http = require("http");
+const url = require("url");
+const querystring = require("querystring");
 const fs = require("fs");
 const mime = require("mime");
 const {v4: uuid} = require("uuid");
+const moment = require("moment");
 
 const PUBLIC_DIR = "./public";
 const PORT = process.env.PORT || 3000;
 
-const appData = [
-	{id: uuid(), name: "John Smith"}
+let users = [
+	{id: uuid(), name: "John Smith", email: "jsmith@gmail.com", dob: "11/26/1992", age: "27"}
 ];
 
 http.createServer((req, res) => {
-	if (req.method === "GET") {
-		if (req.url === "/api/data") {
-			res.writeHeader(200, {"Content-Type": "application/json"});
-			res.end(JSON.stringify(appData));
-		} else {
-			const file = req.url === "/" ? PUBLIC_DIR + "/index.html" : PUBLIC_DIR + req.url;
-			fs.readFile(file, (err, content) => {
-				if (err) {
-					res.writeHeader(404);
-					res.end("Error 404: File Not Found.");
-					return;
-				}
+	switch (req.method) {
+		case "GET": {
+			handleGet(req, res);
+			break;
+		}
+		case "POST": {
+			handlePost(req, res)
+			break;
+		}
+		case "DELETE": {
+			console.log("del req");
+			handleDelete(req, res)
+			break;
+		}
+		case "PATCH": {
+			handlePatch(req, res)
+			break;
+		}
+		default: {
+			handleInvalidMethod(res);
+		}
+	}
+}).listen(PORT, () => console.log(`Listening on port ${PORT}`));
+
+const handleGet = (req, res) => {
+	if (req.url === "/api/users") {
+		handleSuccess(res);
+	} else {
+		const file = req.url === "/" ? PUBLIC_DIR + "/index.html" : PUBLIC_DIR + req.url;
+		fs.readFile(file, (err, content) => {
+			if (err) {
+				handleResourceNotFound(res);
+			} else {
 				res.writeHeader(200, {"Content-Type": mime.getType(file)});
 				res.end(content);
-			});
-		}
-	} else if (req.method === "POST") {
-		let dataString = "";
-		req.on("data", (data) => dataString += data);
-		req.on("end", () => {
-			const dataEntry = JSON.parse(dataString);
-			dataEntry.id = uuid();
-			appData.push(dataEntry);
-
-			res.writeHead(200, "OK", {"Content-Type": "text/plain"});
-			res.end();
+			}
 		});
-	} else {
-		res.writeHeader(405);
-		res.end("Error 405. Method not allowed.");
 	}
-}).listen(PORT, () => console.log(`Listening on port ${PORT}`)); 
+}
+
+const handlePost = (req, res) => {
+	let dataString = "";
+	req.on("data", (data) => dataString += data);
+	req.on("end", () => {
+		if (req.url === "/api/users") {
+			const user = JSON.parse(dataString);
+			user.id = uuid();
+			user.dob = moment(new Date(user.dob)).add(1, "days").format("MM/DD/YYYY");
+			user.age = calculateUserAge(user.dob);
+			users.push(user);
+			console.log("here")
+			handleSuccess(res);
+		} else {
+			handleResourceNotFound(res);
+		}
+	});
+}
+
+const handlePatch = (req, res) => {
+	const parsedUrl = url.parse(req.url);
+	const query  = querystring.parse(parsedUrl.query);
+
+	if (parsedUrl.pathname === "/api/users") {
+		const {id} = query;
+		if (users.some(user => user.id === id)) {
+			//TODO: update user
+			handleSuccess(res);
+		} else {
+			handleResourceNotFound(res);
+		}
+	} else {
+		handleResourceNotFound(res);
+	}
+}
+
+const handleDelete = (req, res) => {
+	const parsedUrl = url.parse(req.url);
+	const query  = querystring.parse(parsedUrl.query);
+
+	if (parsedUrl.pathname === "/api/users") {
+		const {id} = query;
+		if (users.some(user => user.id === id)) {
+			users = users.filter((user) => user.id != id);
+			handleSuccess(res);
+		} else {
+			handleResourceNotFound(res);
+		}
+	} else {
+		handleResourceNotFound(res);
+	}
+}
+
+const handleSuccess = (res) => {
+	res.writeHeader(200, {"Content-Type": "application/json"});
+	res.end(JSON.stringify(users));
+}
+
+const handleResourceNotFound = (res) => {
+	res.writeHeader(404);
+	res.end("Error 404 Not Found.");
+}
+
+const handleInvalidMethod = (res) => {
+	res.writeHeader(405);
+	res.end("Error 405 Method not allowed.");
+}
+
+const calculateUserAge = (dob) => {
+	return moment().diff(moment(dob, "MM/DD/YYYY"), "years");
+}
