fac9a5620cfdfb86a300bdeecf7407b5e07cce96
Glitch (csmbrad-a2-shortstack) <none>
2020-09-17 03:56:12 +0000

    🎌👱 Updated with Glitch

--- public/js/scripts.js
+++ public/js/scripts.js
@@ -1,3 +1,117 @@
 // Add some Javascript code here, to run on the front end.
 
-console.log("Welcome to assignment 2!")
+console.log("Welcome to assignment 2!");
+
+
+const submit = function(e) {
+  // prevent default form action from being carried out
+  e.preventDefault();
+
+  //{ pos: 1, name: "Bill", time: 10, diff: 0, done: "Yes" },
+  const input = document.querySelector("#yourname"),
+    time = document.querySelector("#time"),
+    finished = document.querySelector("#loads"),
+    json = {
+      pos: 0,
+      name: input.value,
+      time: time.value,
+      diff: 0,
+      done: finished.value
+    },
+    body = JSON.stringify(json);
+
+  fetch("/submit", {
+    method: "POST",
+    body
+  }).then(function(response) {
+    // do something with the reponse
+    var incomingData = JSON.parse(response);
+    createTable(incomingData);
+  });
+
+  return false;
+};
+
+const edit = function(e) {
+  // prevent default form action from being carried out
+  e.preventDefault();
+
+  const input = document.querySelector("#yourname"),
+    time = document.querySelector("#time"),
+    finished = document.querySelector("#loads"),
+    json = {
+      pos: 0,
+      name: input.value,
+      time: time.value,
+      diff: 0,
+      done: finished.value
+    },
+    body = JSON.stringify(json);
+  fetch("/edit", {
+    method: "POST",
+    body
+  }).then(function(response) {
+    // do something with the reponse
+    var incomingData = JSON.parse(response);
+    createTable(incomingData);
+  });
+
+  return false;
+};
+
+const delFunc = function(e) {
+  // prevent default form action from being carried out
+  e.preventDefault();
+
+  const input = document.querySelector("#yourname"),
+    json = { name: input.value },
+    body = JSON.stringify(json);
+  fetch("/delete", {
+    method: "POST",
+    body
+  }).then(function(response) {
+    // do something with the reponse
+    var incomingData = JSON.parse(response);
+    createTable(incomingData);
+  });
+};
+
+window.onload = function() {
+  firstTime();
+  /*this was meant to try to fetch the data for the first time when the page loads. it doesn't work.*/
+  const submitButton = document.querySelector("#submitButton");
+  const editButton = document.querySelector("#editButton");
+  const delButton = document.querySelector("#delButton");
+
+  submitButton.onclick = submit;
+  editButton.onclick = edit;
+  delButton.onclick = delFunc;
+};
+
+const createTable = function(data) {
+  document.getElementById("lbBody").innerHTML = "";
+  var html = "";
+  data.forEach(function(e, i) {
+    html += "<tr>" + 
+      "<td>" + e.pos + "</td>" +
+      "<td>" + e.name + "</td>" +
+      "<td>" + e.time + "</td>" +
+      "<td>" + e.diff + "</td>" +
+      "<td>" + e.done + "</td>" +
+      "</tr>";
+  });
+  document.getElementById("lbBody").innerHTML = html;
+};
+
+const firstTime = function() {
+  const json = {start:'start'},
+        body = JSON.stringify(json);
+  fetch("/start", {
+    method: "POST",
+    body
+  }).then(function(response) {
+    console.log(response);
+    /*var incomingData = JSON.parse(response);
+    createTable(incomingData);*/
+  });
+}
--- server.improved.js
+++ server.improved.js
@@ -1,72 +1,130 @@
-const http = require( 'http' ),
-      fs   = require( 'fs' ),
-      // IMPORTANT: you must run `npm install` in the directory for this assignment
-      // to install the mime library used in the following line of code
-      mime = require( 'mime' ),
-      dir  = 'public/',
-      port = 3000
+const http = require("http"),
+  fs = require("fs"),
+  mime = require("mime"),
+  dir = "public/",
+  port = 3000;
 
 const appdata = [
-  { 'model': 'toyota', 'year': 1999, 'mpg': 23 },
-  { 'model': 'honda', 'year': 2004, 'mpg': 30 },
-  { 'model': 'ford', 'year': 1987, 'mpg': 14} 
-]
+  { pos: 1, name: "Bill", time: 10, diff: 0, done: "Yes" },
+  { pos: 2, name: "Bob", time: 14, diff: 4, done: "Yes" },
+  { pos: 3, name: "Jeb", time: 36, diff: 26, done: "Yes" }
+];
 
-const server = http.createServer( function( request,response ) {
-  if( request.method === 'GET' ) {
-    handleGet( request, response )    
-  }else if( request.method === 'POST' ){
-    handlePost( request, response ) 
+const server = http.createServer(function(request, response) {
+  if (request.method === "GET") {
+    handleGet(request, response);
+  } else if (request.method === "POST") {
+    handlePost(request, response);
   }
-})
+});
 
-const handleGet = function( request, response ) {
-  const filename = dir + request.url.slice( 1 ) 
+const handleGet = function(request, response) {
+  const filename = dir + request.url.slice(1);
 
-  if( request.url === '/' ) {
-    sendFile( response, 'public/index.html' )
-  }else{
-    sendFile( response, filename )
+  if (request.url === "/") {
+    sendFile(response, "public/index.html");
+  } else if (request.url === "css/style.css") {
+    sendFile(response, "public/css/style.css");
+  } else {
+    sendFile(response, filename);
   }
-}
+};
 
-const handlePost = function( request, response ) {
-  let dataString = ''
+const handlePost = function(request, response) {
+  let dataString = "";
 
-  request.on( 'data', function( data ) {
-      dataString += data 
-  })
+  request.on("data", function(data) {
+    dataString += data;
+    console.log(dataString);
+  });
 
-  request.on( 'end', function() {
-    console.log( JSON.parse( dataString ) )
-
+  request.on("end", function() {
+    let incoming = JSON.parse(dataString);
+    let responseStr = "";
     // ... do something with the data here!!!
+    if (request.url === "/submit") {
+      //new entry
+      addEntry(response, incoming);
+    } else if (request.url === "/edit") {
+      //edit existing entry
+      editEntry(response, incoming);
+    } else if (request.url === "/delete") {
+      //removing an entry
+      deleteEntry(response, incoming);
+    } else if (request.url === "/start") {
+      //send the data we have right away
+      sendAppdata(response);
+    }
+  });
+};
 
-    response.writeHead( 200, "OK", {'Content-Type': 'text/plain' })
-    response.end()
-  })
-}
+const sendFile = function(response, filename) {
+  const type = mime.getType(filename);
 
-const sendFile = function( response, filename ) {
-   const type = mime.getType( filename ) 
+  fs.readFile(filename, function(err, content) {
+    // if the error = null, then we've loaded the file successfully
+    if (err === null) {
+      // status code: https://httpstatuses.com
+      response.writeHeader(200, { "Content-Type": type });
+      response.end(content);
+    } else {
+      // file not found, error code 404
+      response.writeHeader(404);
+      response.end("404 Error: File Not Found");
+    }
+  });
+};
 
-   fs.readFile( filename, function( err, content ) {
+server.listen(process.env.PORT || port);
 
-     // if the error = null, then we've loaded the file successfully
-     if( err === null ) {
+const sortData = function() {
+  //sort the list in terms of which has the lowest time.
+  appdata.sort((e1, e2) => (e1.time > e2.time ? 1 : -1));
+  //assigning positon numbers and calculating time differences
+  for (let i = 0; i < appdata.length; i++) {
+    appdata[i].pos = i + 1;
+    if ((i = 0)) {
+      appdata[i].diff = 0;
+    } else {
+      appdata[i].diff = appdata[i].time - appdata[0].time;
+    }
+  }
+};
 
-       // status code: https://httpstatuses.com
-       response.writeHeader( 200, { 'Content-Type': type })
-       response.end( content )
+const sendAppdata = function(response) {
+  //sortData(); //commenting out sortdata makes it work. no idea why sortdata is bad.
+  let data = JSON.stringify(appdata);
+  response.writeHeader(200, { "Content-Type": "text/plain" });
+  response.end(data);
+};
 
-     }else{
+const addEntry = function(response, input) {
+  appdata.push(input);
+  sendAppdata(response);
+};
 
-       // file not found, error code 404
-       response.writeHeader( 404 )
-       response.end( '404 Error: File Not Found' )
+const editEntry = function(response, input) {
+  //first search for the entry to see if it exists, if it does modify it.
+  let found = false;
+  for (let i = 0; i < appdata.length; i++) {
+    if (appdata[i].name === input.name) {
+      //found it, update
+      found = true;
+      appdata[i] = input;
+    }
+  }
+  sendAppdata(response);
+};
 
-     }
-   })
-}
-
-server.listen( process.env.PORT || port )
+const deleteEntry = function(response, input) {
+  var index = -1;
+  for (var i = 0; i < appdata.length; i++) {
+        if (appdata[i]['name'] === input.name) {
+            index = i;
+        }
+    }
+  if (index > -1) {
+    appdata.splice(index, 1);
+  }
+  sendAppdata(response);
+};
